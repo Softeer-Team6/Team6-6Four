e2eaee570d6c3927fd9fd10ea60635fc9f950e4c
99winnmin <ojysep9987@gmail.com>
2024-02-05 21:03:25 +0900

    :sparkles: Feat : BaseEntity 추상 클래스 구현
    @MappedSuperclass
    BaseEntity 클래스를 Mapped Superclass 로 지정하여, 이 클래스를 상속받는 엔티티들이 BaseEntity 의 속성을 엔티티의 컬럼으로 포함
    @EntityListeners(AuditingEntityListener.class)
    Spring Data JPA 의 AuditingEntityListener 를 사용하여 엔티티의 생성 및 수정 시간을 자동으로 관리
    
    Resolved: #9

---
+++ BE/src/main/java/com/softeer/team6four/global/infrastructure/database/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.softeer.team6four.global.infrastructure.database;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
