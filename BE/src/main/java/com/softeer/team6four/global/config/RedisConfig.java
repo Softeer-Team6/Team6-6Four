7e48cf8b34e61089c6dca7e9ad5279eca08ed210
99winnmin <ojysep9987@gmail.com>
2024-02-05 21:23:16 +0900

    :sparkles: Feat : RedisConfig 구현
    - yml 에 명시된 호스트와 포트를 사용하여 Redis 연결을 설정하기 위해 LettuceConnectionFactory 추가
    - 문자열 직렬화를 간단하게 하기 위해 키와 값 모두에 StringRedisSerializer 를 사용하도록 RedisTemplate 구성
    
    Related to: #8

---
+++ BE/src/main/java/com/softeer/team6four/global/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.softeer.team6four.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    /**
+     * 기존 jpa 의 인터페이스 선언방식으로 redis 을 사용하기 위해서 redisConnectionFactory 를 빈으로 등록한다.
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // timestamp 형식 안따르도록 설정
+        mapper.registerModules(new JavaTimeModule(), new Jdk8Module()); // LocalDateTime 매핑을 위해 모듈 활성화
+        return mapper;
+    }
+}
