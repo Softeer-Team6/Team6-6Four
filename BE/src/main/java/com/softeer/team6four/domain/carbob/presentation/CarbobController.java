a739a020370c3d7b640fb526b6ade9b6b29ac316
99winnmin <ojysep9987@gmail.com>
2024-02-07 01:26:29 +0900

    :sparkles: Feat : 내 카밥 리스트 조회하기(/v1/carbob/my) controller 구현
    - 등록 최신순, 예약 건수 정렬을 위한 MyCarbobSortType 파라미터 추가
    - 다음 데이터를 가져오기 위한 lastCarbobId 파라미터 추가(첫 페이지 진입시 세팅하지 않아도 동작되도록함)
    - 페이지 기본 사이즈 6
    - 로그인 기능 구현 이후 userId 가져오는 코드 추가 필요
    
    Related to: #27

---
+++ BE/src/main/java/com/softeer/team6four/domain/carbob/presentation/CarbobController.java
@@ -0,0 +1,33 @@
+package com.softeer.team6four.domain.carbob.presentation;
+
+import com.softeer.team6four.domain.carbob.application.CarbobSearchService;
+import com.softeer.team6four.domain.carbob.application.response.MyCarbobSummary;
+import com.softeer.team6four.global.response.ResponseDto;
+import com.softeer.team6four.global.response.SliceResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/carbob")
+@RequiredArgsConstructor
+public class CarbobController {
+    private final CarbobSearchService carbobSearchService;
+
+    @GetMapping(value = "/my")
+    public ResponseDto<SliceResponse<MyCarbobSummary>> getMyCarbobList
+        (
+            @RequestParam MyCarbobSortType sortType,
+            @RequestParam(required = false) Long lastCarbobId,
+            @PageableDefault(size = 6) Pageable pageable
+        )
+    {
+        // TODO : UserContextHold 에서 userId 가져와야함
+        Long userId = 1L;
+        return carbobSearchService.findMyCarbobList(userId, sortType, lastCarbobId, pageable);
+    }
+}
