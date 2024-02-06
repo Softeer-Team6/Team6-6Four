662811c1075a4c9edb272375f78ec6b1233c4a89
99winnmin <ojysep9987@gmail.com>
2024-02-06 14:55:16 +0900

    :sparkles: Feat : Payment Entity 및 Repository 구현
    - 모든 컬럼을 Not NULL 로 세팅
    - User Entity 와 N:1 관계(Lazy load) 세팅
    - targetId 가 0이면 충전, 1 이상 정수면 예약 id
    - Payment 기본 JpaRepository 세팅
    
    Related to: #15

---
+++ BE/src/main/java/com/softeer/team6four/domain/payment/command/domain/Payment.java
@@ -0,0 +1,56 @@
+package com.softeer.team6four.domain.payment.command.domain;
+
+import com.softeer.team6four.domain.user.command.domain.User;
+import com.softeer.team6four.global.infrastructure.database.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "payment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
+    private Long paymentId;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Convert(converter = PayTypeConverter.class)
+    @Column(nullable = false)
+    private PayType payType;
+
+    /**
+     * 0: 충전 / 1 이상: 예약 id
+     */
+    @Column(name = "target_id", nullable = false)
+    private Long targetId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Payment(Integer amount, PayType payType, Long targetId, User user) {
+        this.amount = amount;
+        this.payType = payType;
+        this.targetId = targetId;
+        this.user = user;
+    }
+
+}
