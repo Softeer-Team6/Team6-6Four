597ab1f42013479d22d34277448d6abbb574f39f
99winnmin <ojysep9987@gmail.com>
2024-02-06 14:54:10 +0900

    :sparkles: Feat : Notification Entity 및 Repository 구현
    - 모든 컬럼을 Not NULL 로 세팅
    - User Entity 와 N:1 관계(Lazy load) 세팅
    - FcmToken 기본 JpaRepository 세팅
    
    Related to: #15

---
+++ BE/src/main/java/com/softeer/team6four/domain/notification/command/domain/Notification.java
@@ -0,0 +1,43 @@
+package com.softeer.team6four.domain.notification.command.domain;
+
+import com.softeer.team6four.domain.user.command.domain.User;
+import com.softeer.team6four.global.infrastructure.database.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "notification")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long notificationId;
+
+    @Column(length = 50, nullable = false)
+    private String message;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Notification(String message, User user) {
+        this.message = message;
+        this.user = user;
+    }
+
+}
