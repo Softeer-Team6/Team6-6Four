4998c352b202177e3628c86d91442fbf685a1689
99winnmin <ojysep9987@gmail.com>
2024-02-06 15:20:16 +0900

    :sparkles: Feat : Embeddable CarbobInfo 클래스 구현
    - 모든 컬럼을 Not NULL 로 세팅
    - CarbobInfo 는 카밥 설명과 시간당 요금 정보 포함
    
    Related to: #15

---
+++ BE/src/main/java/com/softeer/team6four/domain/carbob/command/domain/CarbobInfo.java
@@ -0,0 +1,31 @@
+package com.softeer.team6four.domain.carbob.command.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CarbobInfo {
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "fee_per_hour", nullable = false)
+    private Integer feePerHour;
+
+    @Builder
+    public CarbobInfo(String description, SpeedType speedType, Integer fee) {
+        this.description = description;
+        this.feePerHour = calculateFee(speedType, fee);
+    }
+
+    private Integer calculateFee(SpeedType speedType, Integer fee) {
+        return Integer.parseInt(speedType.getValue()) * fee;
+    }
+
+}
