1a194367539a1ccc28b92deb6be6fafce0f4769c
99winnmin <ojysep9987@gmail.com>
2024-02-06 14:56:24 +0900

    :sparkles: Feat : Payment 열거형 클래스 및 최상위 Converter 연결
    - Payment 타입을 CHARGE, USE, INCOME 으로 정의
    
    Related to: #15

---
+++ BE/src/main/java/com/softeer/team6four/domain/payment/command/domain/PayType.java
@@ -0,0 +1,29 @@
+package com.softeer.team6four.domain.payment.command.domain;
+
+
+import com.softeer.team6four.global.infrastructure.database.CodeValue;
+
+public enum PayType implements CodeValue {
+    CHARGE("CHARGE", "포인트 충전"),
+    USE("USE", "포인트 사용"),
+    INCOME("INCOME", "포인트 수입"),;
+
+    private final String code;
+    private final String value;
+
+    PayType(String code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+}
