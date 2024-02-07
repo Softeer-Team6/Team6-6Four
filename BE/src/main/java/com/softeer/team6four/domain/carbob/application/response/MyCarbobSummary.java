79a8910b41fb9f030ffaecc220f3adc3b62774d1
99winnmin <ojysep9987@gmail.com>
2024-02-07 13:40:23 +0900

    :sparkles: Feat : 내 카밥 요약 정보 response 클래스 추가
    - 클래스 필드로 carbobId(카밥 id), nickname(카밥 닉네임), imageUrl(카밥 이미지 URL), reservationCount(예약 횟수)를 포함
    - 예약 횟수는 WAIT 상태만을 카운트함
    - 모든 필드는 @NotNull 어노테이션을 사용하여 null이 아님을 명시, 데이터의 무결성 보장
    - @Getter 및 @AllArgsConstructor 어노테이션을 사용하여 필드에 대한 getter 메소드와 모든 필드를 초기화하는 생성자 자동 생성
    
    Related to: #27

---
+++ BE/src/main/java/com/softeer/team6four/domain/carbob/application/response/MyCarbobSummary.java
@@ -0,0 +1,14 @@
+package com.softeer.team6four.domain.carbob.application.response;
+
+import com.google.firebase.database.annotations.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MyCarbobSummary {
+    private final @NotNull Long carbobId;
+    private final @NotNull String nickname;
+    private final @NotNull String imageUrl;
+    private final @NotNull Long reservationCount;
+}
