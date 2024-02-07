bd6e21fb3ac8492a3c0fc8ddf28445b1a5c73b25
kimseongwooo <tjddn929@naver.com>
2024-02-07 10:10:51 +0900

    :sparkles: Feat :  홈 화면 UI 구성 - HomeFragment 클래스 생성 - 상단에 메뉴 버튼 배치 - 상단에 검색창 배치 - 하단에 카메라 버튼 배치 - 하단에 내 위치 근처 충전소 버튼 배치 - 하단에 현재 위치 버튼 배치 - 중앙에 네이버 맵 배치
    Related to: #19

--- AOS/app/src/main/java/com/softeer/team6four/MainActivity.kt
+++ AOS/app/src/main/java/com/softeer/team6four/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.softeer.team6four
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.softeer.team6four.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        supportFragmentManager.beginTransaction().add(
+            binding.fragContainer.id, HomeFragment()
+        ).commit()
     }
 }
