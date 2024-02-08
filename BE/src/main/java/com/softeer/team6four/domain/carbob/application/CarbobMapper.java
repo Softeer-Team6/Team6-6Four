f8c25dde66a1d73e003974beed37f30551229335
99winnmin <ojysep9987@gmail.com>
2024-02-08 18:47:19 +0900

    :sparkles: feat : 카밥 상세 정보 조회 dto mapper 추가
    - Carbob Entity 와 따로 조회한 값들을 이용해서 MyCarbobDetailInfo 빌더를 구성해서 반환하는 정적 메서드 추가
    
    Related to: #32

---
+++ BE/src/main/java/com/softeer/team6four/domain/carbob/application/CarbobMapper.java
@@ -0,0 +1,24 @@
+package com.softeer.team6four.domain.carbob.application;
+
+import com.softeer.team6four.domain.carbob.application.response.MyCarbobDetailInfo;
+import com.softeer.team6four.domain.carbob.domain.Carbob;
+import com.softeer.team6four.domain.reservation.application.SelfUseTime;
+
+public class CarbobMapper {
+    public static MyCarbobDetailInfo mapMyCarbobDetailInfo(Carbob carbob, String carbobImageUrl, SelfUseTime selfUseTime, Long totalIncomeByTargetId) {
+        return MyCarbobDetailInfo.builder()
+            .carbobId(carbob.getCarbobId())
+            .nickname(carbob.getHost().getNickname())
+            .imageUrl(carbobImageUrl)
+            .address(carbob.getLocation().getAddress())
+            .feePerHour(String.valueOf(carbob.getInfo().getFeePerHour()).concat("원/kwh"))
+            .chargerType(carbob.getSpec().getChargerType().getValue())
+            .speedType(carbob.getSpec().getSpeedType().getValue())
+            .installType(carbob.getSpec().getInstallType().getValue())
+            .description(carbob.getInfo().getDescription())
+            .selfUseTime(selfUseTime.toString())
+            .carbobTotalIncome(totalIncomeByTargetId)
+            .build();
+    }
+
+}
