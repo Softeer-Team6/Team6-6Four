6b99085006314afd2136923dd532ca056934fba4
99winnmin <ojysep9987@gmail.com>
2024-02-08 18:25:59 +0900

    :sparkles: feat : PaymentRepository - findPaymentsByPayTypeAndTargetIdIn 메서드 추가
    - 카밥으로 벌어들인 소득 내역을 가져오기 위한 PayType, targetIdList 로 Payment List 조회 JPA 추가
    
    Related to: #32

--- BE/src/main/java/com/softeer/team6four/domain/payment/domain/PaymentRepository.java
+++ BE/src/main/java/com/softeer/team6four/domain/payment/domain/PaymentRepository.java
@@ -1,8 +1,8 @@
 package com.softeer.team6four.domain.payment.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByUser_UserId(Long id);
