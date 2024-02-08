b38f5998d1754f266b589535964ef7fe8ecb6c77
99winnmin <ojysep9987@gmail.com>
2024-02-08 18:30:02 +0900

    :sparkles: feat : stateType 과 carbobId 에 따른 Reservation 단건, 리스트 조회 JPA 추가
    - 리스트 조회는 괜찮으나 findReservationByCarbob_CarbobIdAndStateType 단건 조회 JPA 가 예상대로 동작하는지 검증 필요
    
    Related to: #32

--- BE/src/main/java/com/softeer/team6four/domain/reservation/domain/ReservationRepository.java
+++ BE/src/main/java/com/softeer/team6four/domain/reservation/domain/ReservationRepository.java
@@ -1,7 +1,10 @@
 package com.softeer.team6four.domain.reservation.domain;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
+    Optional<Reservation> findReservationByCarbob_CarbobIdAndStateType(Long carbobId, StateType stateType);
+    List<Reservation> findAllByCarbob_CarbobIdAndStateType(Long carbobId, StateType stateType);
 }
