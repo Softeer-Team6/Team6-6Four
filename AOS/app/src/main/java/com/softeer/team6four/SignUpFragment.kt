2507d36d6324ca41adbeb5f071dc633b6362a08c
kimseongwooo <tjddn929@naver.com>
2024-02-08 20:59:16 +0900

    :art: Fix : Binding 변수를 통한 SignUpFragment 바인딩
    - 생성한 binding 변수를 통해서 SignUpFragment 바인딩하는 방식으로 수정
    
    Related to: #41

--- AOS/app/src/main/java/com/softeer/team6four/SignUpFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/SignUpFragment.kt
@@ -1,18 +1,23 @@
 package com.softeer.team6four
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.softeer.team6four.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
