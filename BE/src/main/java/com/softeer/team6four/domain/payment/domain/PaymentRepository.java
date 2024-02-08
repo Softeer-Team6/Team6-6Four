f9109c79e27d90b2b2a4c337012ae4bef427ad15
99winnmin <ojysep9987@gmail.com>
2024-02-08 22:45:55 +0900

    :sparkles: feat : carbobId 에 대한 총 소득 반환 메서드 추가
    - carbobId 로 사용완료된 예약 id 검색해서 Payment 중 INCOME 된 데이터 리스트 조회
    - Payment 리스트의 amount 합산
    
    Resolved: #32

--- BE/src/main/java/com/softeer/team6four/domain/payment/domain/PaymentRepository.java
+++ BE/src/main/java/com/softeer/team6four/domain/payment/domain/PaymentRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByUser_UserId(Long id);
+    List<Payment> findPaymentsByPayTypeAndTargetIdIn(PayType payType, List<Long> targetIdList);
 }
