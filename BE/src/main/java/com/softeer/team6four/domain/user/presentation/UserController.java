4fe844c05c75e9cf59e52a4ac4d8309562d83f49
yeriinnn <angelrin7@naver.com>
2024-02-10 00:21:16 +0900

    feat: UserContextHolder 클래스 추가
    - ThreadLocal에 사용자 정보 저장 및 관리
    - 사용자 정보를 유지하기 위해 사용
    
    Related to: #20

--- BE/src/main/java/com/softeer/team6four/domain/user/presentation/UserController.java
+++ BE/src/main/java/com/softeer/team6four/domain/user/presentation/UserController.java
@@ -2,23 +2,26 @@ package com.softeer.team6four.domain.user.presentation;
 
 
 import com.softeer.team6four.domain.user.application.EmailService;
+import com.softeer.team6four.domain.user.application.UserJoinService;
 import com.softeer.team6four.domain.user.application.response.EmailCheck;
+import com.softeer.team6four.domain.user.application.response.SignUpRequest;
 import com.softeer.team6four.global.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/user")
 @RequiredArgsConstructor
 public class UserController {
     private final EmailService emailService;
+    private final UserJoinService userJoinService;
 
     @GetMapping(value = "/email/check")
     public ResponseDto<EmailCheck> checkEmail(@RequestParam String email) {
         return emailService.findEmail(email);
     }
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody SignUpRequest signupRequest) {userJoinService.signup(signupRequest);}
 }
 
