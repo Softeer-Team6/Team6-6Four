a9d841f860c773431b3111da7270cf4f90723187
99winnmin <ojysep9987@gmail.com>
2024-02-10 02:51:16 +0900

    :sparkles: feat : carbobId 에 대해서 예약 시간 있는지 체크하는 query 추가
    - carbobId 에 대해서 예약시간 리스트 중에 하나라도 존재하면 true, 없으면 false 반환하도록 구현
    
    Related to: #53

--- BE/src/main/java/com/softeer/team6four/domain/reservation/infra/ReservationRepositoryImpl.java
+++ BE/src/main/java/com/softeer/team6four/domain/reservation/infra/ReservationRepositoryImpl.java
@@ -7,13 +7,17 @@ import static com.softeer.team6four.domain.reservation.domain.QReservationLine.r
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.softeer.team6four.domain.reservation.application.ReservationTime;
 import com.softeer.team6four.domain.reservation.application.response.ReservationInfo;
 import com.softeer.team6four.domain.reservation.domain.Reservation;
+import com.softeer.team6four.domain.reservation.domain.ReservationLine;
 import com.softeer.team6four.domain.reservation.domain.StateType;
 import com.softeer.team6four.domain.reservation.presentation.ReservationStateSortType;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -68,6 +72,34 @@ public class ReservationRepositoryImpl extends QuerydslRepositorySupport {
             .fetch();
 
         return checkLastPage(pageable, results);
+    }
+
+    public boolean existsByCarbobIdAndReservationLines(Long carbobId, List<ReservationLine> newReservationLines) {
+        // 동적으로 조건을 생성하기 위한 BooleanExpression 리스트
+        List<BooleanExpression> timeConditions = new ArrayList<>();
+
+        // reservationTimes 리스트를 순회하면서 각 ReservationTime 에 대한 조건 생성
+        for (ReservationLine time : newReservationLines) {
+            LocalDateTime newReservationTime = time.getReservationTime();
+            BooleanExpression timeCondition = reservation.reservationLines.any().reservationTime.eq(newReservationTime);
+            timeConditions.add(timeCondition);
+        }
+
+        BooleanExpression combinedCondition = Expressions.FALSE;
+        for (BooleanExpression condition : timeConditions) {
+            combinedCondition = combinedCondition.or(condition);
+        }
+
+        // 겹치는 것이 하나라도 있으면 true, 없으면 false 리턴
+        return queryFactory
+            .selectOne()
+            .from(reservation)
+            .where(
+                reservation.carbob.carbobId.eq(carbobId),
+                reservation.stateType.ne(StateType.REJECT),
+                combinedCondition
+            )
+            .fetchFirst() != null;
     }
 
     private BooleanExpression ltReservationId(Long reservationId) {
