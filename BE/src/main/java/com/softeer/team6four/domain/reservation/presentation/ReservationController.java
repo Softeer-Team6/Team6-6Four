cc648f897084f39afb81246377b5e2065a24817a
99winnmin <ojysep9987@gmail.com>
2024-02-10 02:52:38 +0900

    :sparkles: feat : 예약 요청 API 추가
    - carbobId 와 시작,끝 시간 을 body 로 받아서 성공하면 200 코드와 null body 를 반환하도록 설정
    
    Related to: #53

--- BE/src/main/java/com/softeer/team6four/domain/reservation/presentation/ReservationController.java
+++ BE/src/main/java/com/softeer/team6four/domain/reservation/presentation/ReservationController.java
@@ -1,13 +1,18 @@
 package com.softeer.team6four.domain.reservation.presentation;
 
+import com.softeer.team6four.domain.reservation.application.ReservationCreateService;
 import com.softeer.team6four.domain.reservation.application.ReservationSearchService;
+import com.softeer.team6four.domain.reservation.application.request.ReservationApply;
 import com.softeer.team6four.domain.reservation.application.response.ReservationInfo;
 import com.softeer.team6four.global.response.ResponseDto;
 import com.softeer.team6four.global.response.SliceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
 
     private final ReservationSearchService reservationSearchService;
+    private final ReservationCreateService reservationCreateService;
 
     @GetMapping("/application/list")
     public ResponseDto<SliceResponse<ReservationInfo>> getMyReservationApplicationList
@@ -30,5 +36,17 @@ public class ReservationController {
         // TODO : UserContextHold 에서 userId 가져오기
         Long userId = 3L;
         return reservationSearchService.getMyReservationApplicationList(userId, sortType, lastReservationId, pageable);
+    }
+
+    @PostMapping("/apply")
+    public ResponseDto<Void> applyReservation
+        (
+            @RequestBody ReservationApply reservationApply
+        )
+    {
+        // TODO : UserContextHold 에서 userId 가져오기
+        Long userId = 2L;
+        reservationCreateService.makeReservationToCarbobV1(userId, reservationApply);
+        return ResponseDto.map(HttpStatus.OK.value(), "예약 신청이 완료되었습니다.", null);
     }
 }
