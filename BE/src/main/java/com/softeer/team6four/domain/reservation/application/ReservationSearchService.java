321c2f4ac8fb319f2393c391f8d2cba23cc81177
99winnmin <ojysep9987@gmail.com>
2024-02-09 20:18:25 +0900

    :sparkles: feat : 내 예약 신청 내역 조회 service 메서드 추가
    - userId, sortType, lastReservationId 으로 예약 내역을 no-offset 으로 호출하는 메서드로 Slice 값을 받아옴
    - ReservationInfo 각각의 값에 대해 예약 시간을 String 으로 변환 후 리턴
    
    Related to: #50

--- BE/src/main/java/com/softeer/team6four/domain/reservation/application/ReservationSearchService.java
+++ BE/src/main/java/com/softeer/team6four/domain/reservation/application/ReservationSearchService.java
@@ -1,14 +1,22 @@
 package com.softeer.team6four.domain.reservation.application;
 
+import com.softeer.team6four.domain.reservation.application.response.ReservationInfo;
 import com.softeer.team6four.domain.reservation.domain.Reservation;
 import com.softeer.team6four.domain.reservation.domain.ReservationLine;
 import com.softeer.team6four.domain.reservation.domain.ReservationRepository;
 import com.softeer.team6four.domain.reservation.domain.StateType;
+import com.softeer.team6four.domain.reservation.infra.ReservationRepositoryImpl;
+import com.softeer.team6four.domain.reservation.presentation.ReservationStateSortType;
+import com.softeer.team6four.global.response.ResponseDto;
+import com.softeer.team6four.global.response.SliceResponse;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +25,20 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ReservationSearchService {
     private final ReservationRepository reservationRepository;
+    private final ReservationRepositoryImpl reservationRepositoryImpl;
+
+    public ResponseDto<SliceResponse<ReservationInfo>> getMyReservationApplicationList
+        (
+            Long userId,
+            ReservationStateSortType sortType,
+            Long lastReservationId,
+            Pageable pageable
+        )
+    {
+        Slice<ReservationInfo> reservationInfoList = reservationRepositoryImpl.findReservationInfoList(userId, sortType, lastReservationId, pageable);
+        reservationInfoList.forEach(ReservationInfo::convertReservationTimeToStr);
+        return ResponseDto.map(HttpStatus.OK.value(), "예약 내역 조회에 성공했습니다.", SliceResponse.of(reservationInfoList));
+    }
 
     public SelfUseTime getSelfUseTime(Long carbobId) {
         Reservation reservation = reservationRepository
