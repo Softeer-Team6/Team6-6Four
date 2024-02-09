4c27e6f179a402d4253c9fb7bbaf987fbe2644ae
99winnmin <ojysep9987@gmail.com>
2024-02-09 20:31:38 +0900

    :sparkles: feat : 예약 정보 반환 response dto 추가
    - carbobId, reservationTimeStr, totalFee, nickname 등 필요한 데이터 구성
    - ReservationTime 객체는 ReservationInfo 내에서 사용되는 객체로 QueryDsl 내에서 사용
    
    Related to: #50

---
+++ BE/src/main/java/com/softeer/team6four/domain/reservation/application/ReservationTime.java
@@ -0,0 +1,24 @@
+package com.softeer.team6four.domain.reservation.application;
+
+import com.google.firebase.database.annotations.NotNull;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ReservationTime {
+    private final @NotNull LocalDateTime startTime;
+    private final @NotNull LocalDateTime endTime;
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        String date = startTime.format(DateTimeFormatter.ofPattern("MM-dd"));
+        String hours = startTime.format(formatter) + " ~ " + endTime.plusMinutes(59).format(formatter);
+        return date + " " + hours;
+    }
+}
