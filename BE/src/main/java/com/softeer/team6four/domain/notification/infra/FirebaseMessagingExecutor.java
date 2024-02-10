11d28dd31b979133e72c40356b9231e5ad672da4
99winnmin <ojysep9987@gmail.com>
2024-02-10 18:31:24 +0900

    :sparkles: feat : firebase FCM 단일 및 다중 사용자 알림 실행기 구현
    - Android 기기에 대한 구성(AndroidConfig)을 설정
    - sendFCMNotificationSingle : 주어진 FCM 토큰(fcmToken), 제목(title), 본문(body)을 사용하여 단일 사용자에게 알림을 비동기 전송
    - sendFCMNotificationMulticast : 여러 사용자의 FCM 토큰 목록(fcmTokens), 제목(title), 본문(body)을 사용하여 다중 사용자에게 알림을 비동기 전송
    
    Related to: #61

---
+++ BE/src/main/java/com/softeer/team6four/domain/notification/infra/FirebaseMessagingExecutor.java
@@ -0,0 +1,58 @@
+package com.softeer.team6four.domain.notification.infra;
+
+import com.google.firebase.messaging.AndroidConfig;
+import com.google.firebase.messaging.AndroidNotification;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MulticastMessage;
+import com.google.firebase.messaging.Notification;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FirebaseMessagingExecutor {
+    private final FirebaseMessaging firebaseMessaging;
+
+    // Android 세팅
+    private AndroidConfig androidConfig(String title, String body) {
+        return AndroidConfig.builder()
+                .setNotification(AndroidNotification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                .build();
+    }
+
+    public void sendFCMNotificationSingle(String fcmToken, String title, String body) {
+        Notification notification = Notification.builder()
+                .setTitle(title).setBody(body).build();
+
+        Message message = Message.builder()
+                .setAndroidConfig(androidConfig(title, body))
+                .setNotification(notification)
+                .setToken(fcmToken)
+                .build();
+        try {
+            firebaseMessaging.sendAsync(message);
+        } catch (Exception e) {
+            log.error("Failed to send FCM notification", e);
+        }
+    }
+
+    public void sendFCMNotificationMulticast(List<String> fcmTokens, String title, String body) {
+        Notification notification = Notification.builder()
+                .setTitle(title).setBody(body).build();
+
+        MulticastMessage multicastMessage = MulticastMessage.builder()
+                .setNotification(notification)
+                .setAndroidConfig(androidConfig(title, body))
+                .addAllTokens(fcmTokens)
+                .build();
+
+        firebaseMessaging.sendMulticastAsync(multicastMessage);
+    }
+}
