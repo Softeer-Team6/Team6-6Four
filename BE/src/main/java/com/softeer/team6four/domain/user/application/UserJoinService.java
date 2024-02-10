8d37b0bd24a8928dd1633b860d577ddea232f322
yeriinnn <angelrin7@naver.com>
2024-02-10 23:21:15 +0900

    feat: UserJoinService 내에 로그인 관련 추가
    - 유저의 로그인 성공시 생성된 JWT 토큰에 대해 클라이언트에게 반환
    - 유저 이메일 기반으로 DB에서 조회
    - 조회된 유저 없는 경우 예외 발생
    - JWTProvider 사용해 유저 ID 기반의 액세스 토큰 및 리프레시 토큰 생성
    - 생성된 액세스 토큰과 리프레시 토큰을 이용해 response 객체 생성
    
    Related to: #47

--- BE/src/main/java/com/softeer/team6four/domain/user/application/UserJoinService.java
+++ BE/src/main/java/com/softeer/team6four/domain/user/application/UserJoinService.java
@@ -1,10 +1,16 @@
 package com.softeer.team6four.domain.user.application;
 
+import com.softeer.team6four.domain.user.application.exception.UserNotFoundException;
+import com.softeer.team6four.domain.user.application.request.SignInRequest;
 import com.softeer.team6four.domain.user.application.request.SignUpRequest;
+import com.softeer.team6four.domain.user.application.response.SignInJwtResponse;
 import com.softeer.team6four.domain.user.domain.User;
 import com.softeer.team6four.domain.user.domain.UserRepository;
 import com.softeer.team6four.global.auth.JwtProvider;
+import com.softeer.team6four.global.response.ErrorCode;
+import com.softeer.team6four.global.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,5 +30,22 @@ public class UserJoinService {
         userRepository.save(newUser);
 
     }
+
+    @Transactional(readOnly = true)
+    public ResponseDto<SignInJwtResponse> signin(SignInRequest signInRequest) {
+        User user = userRepository.findUserByEmail(signInRequest.getEmail())
+                .orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
+
+        String accessToken = jwtProvider.generateAccessToken(user.getUserId());
+        String refreshToken = jwtProvider.generateRefreshToken(user.getUserId());
+
+        SignInJwtResponse signInJwtResponse = SignInJwtResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+        return ResponseDto.map(HttpStatus.OK.value(),"로그인에 성공했습니다.", signInJwtResponse);
+
+    }
+
 
 }
