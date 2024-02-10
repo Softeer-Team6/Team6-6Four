b63abb81e8fff6ef5e29cd907bb8b36ce5a4f64d
yeriinnn <angelrin7@naver.com>
2024-02-10 23:24:50 +0900

    feat: UserRepository 내에 유저 조회 추가
    - 이메일 기반으로 유저 정보 조회
    - 첫 화면 == 로그인 화면이기 때문에 회원 가입을 하지 않은 유저가 로그인 시도 하는 경우에 대비하기 위해 optional 로 감싸서 반환
    
    Related to: #47

--- BE/src/main/java/com/softeer/team6four/domain/user/domain/UserRepository.java
+++ BE/src/main/java/com/softeer/team6four/domain/user/domain/UserRepository.java
@@ -3,7 +3,10 @@ package com.softeer.team6four.domain.user.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
+    Optional<User> findUserByEmail(String email);
 }
