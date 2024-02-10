84e7a0e39cd9b4ae11f11fb2d08719a434dea2f8
DSL2e <kdb2013@g.skku.edu>
2024-02-10 14:51:41 +0900

    :sparkles:  feat: 포인트내역조회 query 구현
    - 사용자 ID(userId), 마지막 포인트 ID(lastPaymentId), 페이지 정보(Pageable)를 파라미터로 받는다
    - Querydsl 을 사용하여 MyPointSummary를 만드는데, PayType이 INCOME이라면 targetId를 저장하여 고객의 nickName를 저장하는데 사용하고 다른 payType의 targetId는 0으로 설정
    - ltReservationId으로 마지막 paymentId보다 작은 부분만 가져는 방식으로 누적되는 방식이 아닌 on-offset으로 가져옴
    - 현재페이지에 +1를 한 후, pageable.getPageSize()보다 크다면 다음 페이가 있다고 판단
    
    Related to : #45

---
+++ BE/src/main/java/com/softeer/team6four/domain/payment/infra/PaymentRepositoryImpl.java
@@ -0,0 +1,79 @@
+package com.softeer.team6four.domain.payment.infra;
+
+import static com.softeer.team6four.domain.payment.domain.QPayment.payment;
+import static com.softeer.team6four.domain.user.domain.QUser.user;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.softeer.team6four.domain.payment.application.response.MyPointSummary;
+import com.softeer.team6four.domain.payment.domain.PayType;
+import com.softeer.team6four.domain.payment.domain.Payment;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PaymentRepositoryImpl extends QuerydslRepositorySupport {
+    private final JPAQueryFactory queryFactory;
+
+    public PaymentRepositoryImpl(JPAQueryFactory queryFactory) {
+        super(Payment.class);
+        this.queryFactory = queryFactory;
+    }
+
+    public Slice<MyPointSummary> findMyPointSummaryList(Long userId, Long lastPaymentId, Pageable pageable) {
+        JPAQuery<MyPointSummary> query = queryFactory
+                .select(Projections.constructor(
+                            MyPointSummary.class,
+                            payment.paymentId,
+                            payment.amount,
+                            payment.createdDate,
+                            payment.payType,
+                            Expressions.cases()
+                                    .when(payment.payType.eq(PayType.INCOME)).then(payment.targetId) // targetId를 통해 user닉네임을 가져온다
+                                    .otherwise(0L) // 기본값으로 0을 반환
+                                    .as("targetId")
+                            )
+
+                )
+                .from(payment)
+                .leftJoin(payment.user, user)
+                .where(
+                        payment.user.userId.eq(userId),
+                        ltPaymentId(lastPaymentId)
+                )
+                .orderBy(payment.createdDate.desc());
+
+        List<MyPointSummary> results = query
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        return checkLastPage(pageable, results);
+    }
+
+    private BooleanExpression ltPaymentId(Long paymentId) {
+        if (paymentId == null) {
+            return null;
+        }
+        return payment.paymentId.lt(paymentId);
+    }
+
+    private Slice<MyPointSummary> checkLastPage(Pageable pageable, List<MyPointSummary> results) {
+        boolean hasNext = false;
+
+        if (results.size() > pageable.getPageSize()) {
+            hasNext = true;
+            results.remove(pageable.getPageSize());
+        }
+
+        return new SliceImpl<>(results, pageable, hasNext);
+    }
+
+}
