55073152645356aff703dc36f37cdc04abc4f8a6
kimseongwooo <tjddn929@naver.com>
2024-02-11 17:18:27 +0900

    :lipstick: Feat : RegisterOnboardingFragment -> ConditionCheckFragment 화면 전환 작업 - fragment_register_onboarding.xml 버튼 id 추가 - register_graph.xml 파일 화면 전환 작업을 위한 action 추가 - RegisterOnboardingFragment 클래스에서 버튼 클릭 시 화면 전환 기능 구현
    Related to: #63

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterOnboardingFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterOnboardingFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.softeer.team6four.R
 import com.softeer.team6four.databinding.FragmentRegisterOnboardingBinding
 
 class RegisterOnboardingFragment : Fragment() {
@@ -18,6 +20,11 @@ class RegisterOnboardingFragment : Fragment() {
     ): View {
         _binding = FragmentRegisterOnboardingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNext.setOnClickListener { findNavController().navigate(R.id.action_registerOnboardingFragment_to_conditionCheckFragment) }
     }
 
     override fun onDestroy() {
