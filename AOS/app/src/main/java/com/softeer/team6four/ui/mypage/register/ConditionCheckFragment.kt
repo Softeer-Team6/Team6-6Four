232018cc2f0ab4b6bbae1bcba0822e6ce2fb90d3
kimseongwooo <tjddn929@naver.com>
2024-02-11 20:31:17 +0900

    :lipstick: Feat : ConditionCheckFragment -> RegisterLocationFragment 화면 전환 기능 구현 - register_graph.xml 파일에서 RegisterLocationFragment destination 추가 - 조건 확인 화면에서 다음 버튼 클릭 시 카밥 위치 등록 화면 등록 화면 전환 기능 구현
    Related to: #63

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/ConditionCheckFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/ConditionCheckFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.softeer.team6four.R
 import com.softeer.team6four.databinding.FragmentConditionCheckBinding
 
 
@@ -15,11 +17,15 @@ class ConditionCheckFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentConditionCheckBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNext.setOnClickListener { findNavController().navigate(R.id.action_conditionCheckFragment_to_registerLocationFragment) }
+    }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
