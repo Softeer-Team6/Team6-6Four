58200799a0f87691d73e2bf95348a6c426a6f69e
kimseongwooo <tjddn929@naver.com>
2024-02-11 23:29:23 +0900

    :lipstick: Feat : RegisterLocationFragment -> RegisterDetailFragment 화면 전환 기능 구현 - register_graph.xml 파일에 destination으로 RegisterDetailFragment 추가 - register_graph.xml 파일에 화면 전환 action 추가 - RegisterLocationFragment 버튼 클릭시 화면 전환 기능 구현
    Related to: #63

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterLocationFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterLocationFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.softeer.team6four.R
 import com.softeer.team6four.databinding.FragmentRegisterLocationBinding
 
 
@@ -19,6 +21,11 @@ class RegisterLocationFragment : Fragment() {
     ): View {
         _binding = FragmentRegisterLocationBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNext.setOnClickListener { findNavController().navigate(R.id.action_registerLocationFragment_to_registerDetailFragment) }
     }
 
     override fun onDestroy() {
