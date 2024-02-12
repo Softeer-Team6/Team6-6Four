cb0fdde46660dd3ae1100577845de373fbecac9b
kimseongwooo <tjddn929@naver.com>
2024-02-12 18:49:25 +0900

    :lipstick: Feat : RegisterTimeFragment 클래스 파일 생성 - 공유 불가능 시간대 등록을 위한 RegisterTimeFragment 클래스 생성 - binding 변수를 통한 프래그먼트 바인딩 Related to: #63

---
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterTimeFragment.kt
@@ -0,0 +1,27 @@
+package com.softeer.team6four.ui.mypage.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.softeer.team6four.databinding.FragmentRegisterTimeBinding
+
+class RegisterTimeFragment : Fragment() {
+    private var _binding: FragmentRegisterTimeBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRegisterTimeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
