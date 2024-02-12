d0d8c9a106613d3e71e579936c268de46dc042c9
yeriinnn <angelrin7@naver.com>
2024-02-12 21:11:27 +0900

    feat: UserController 내에 기능 추가
    - 닉네임 중복 체크 관련 내용
    
    Related to: #36

--- BE/src/main/java/com/softeer/team6four/domain/user/presentation/UserController.java
+++ BE/src/main/java/com/softeer/team6four/domain/user/presentation/UserController.java
@@ -2,10 +2,12 @@ package com.softeer.team6four.domain.user.presentation;
 
 
 import com.softeer.team6four.domain.user.application.EmailService;
+import com.softeer.team6four.domain.user.application.NicknameService;
 import com.softeer.team6four.domain.user.application.UserJoinService;
 import com.softeer.team6four.domain.user.application.response.EmailCheck;
 import com.softeer.team6four.domain.user.application.request.SignUpRequest;
 import com.softeer.team6four.domain.user.application.request.SignInRequest;
+import com.softeer.team6four.domain.user.application.response.NicknameCheck;
 import com.softeer.team6four.domain.user.application.response.SignInJwtResponse;
 import com.softeer.team6four.global.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +18,17 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final EmailService emailService;
+    private final NicknameService nicknameService;
     private final UserJoinService userJoinService;
 
     @GetMapping(value = "/email/check")
     public ResponseDto<EmailCheck> checkEmail(@RequestParam String email) {
         return emailService.findEmail(email);
+    }
+
+    @GetMapping(value = "/nickname/check")
+    public ResponseDto<NicknameCheck> checkNickname(@RequestParam String nickname) {
+        return nicknameService.findNickname(nickname);
     }
 
     @PostMapping("/auth/signup")
