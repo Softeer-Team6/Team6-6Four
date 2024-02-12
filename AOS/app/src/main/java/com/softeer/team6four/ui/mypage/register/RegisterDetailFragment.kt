54dec5d7df906d0b816afc6cb062db451dd5e0ee
kimseongwooo <tjddn929@naver.com>
2024-02-12 14:03:24 +0900

    :lipstick: Feat : RegisterDetailFragment -> RegisterPriceFragment 화면 전환 기능 구현 - register_graph.xml 파일에 destination으로 RegisterPriceFragment 추가 - RegisterDetailFragment에서 RegisterPriceFragment로 이동하는 액션 추가 - RegisterDetailFragment의 다음 버튼 클릭 시 화면 전환 기능 구현
    Related to: #63

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterDetailFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterDetailFragment.kt
@@ -1,23 +1,30 @@
 package com.softeer.team6four.ui.mypage.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.softeer.team6four.R
 import com.softeer.team6four.databinding.FragmentRegisterDetailBinding
 
 class RegisterDetailFragment : Fragment() {
-    private var _binding : FragmentRegisterDetailBinding? = null
+    private var _binding: FragmentRegisterDetailBinding? = null
     private val binding
         get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentRegisterDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNext.setOnClickListener { findNavController().navigate(R.id.action_registerDetailFragment_to_registerPriceFragment) }
     }
 
     override fun onDestroy() {
