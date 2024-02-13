2770b1a525a1ae282ad897b9e4b0c21681ff21d1
DSL2e <kdb2013@g.skku.edu>
2024-02-13 18:56:41 +0900

    :fire: fix: rebase 충돌 해결
    - rebase 과정에서 충돌이 발생하여 현재 dev브랜치를 돌린 후, 다시 코드 작성하였습니다
    
    Related to : #73

--- BE/src/main/java/com/softeer/team6four/domain/carbob/application/AroundCarbobSearchService.java
+++ BE/src/main/java/com/softeer/team6four/domain/carbob/application/AroundCarbobSearchService.java
@@ -1,8 +1,10 @@
 package com.softeer.team6four.domain.carbob.application;
 
+import com.softeer.team6four.domain.carbob.application.response.AroundCarbobListInfo;
 import com.softeer.team6four.domain.carbob.application.response.AroundCarbobListInfoSummary;
-import com.softeer.team6four.domain.carbob.domain.CarbobRepository;
 import com.softeer.team6four.domain.carbob.infra.AroundCarbobRepositoryImpl;
+import com.softeer.team6four.domain.carbob.presentation.CarbobListStateSortType;
+import com.softeer.team6four.global.response.ListResponse;
 import com.softeer.team6four.global.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,11 +19,18 @@ import java.util.List;
 public class AroundCarbobSearchService {
     private final AroundCarbobRepositoryImpl  aroundCarbobRepositoryImpl;
 
-    public ResponseDto<List<AroundCarbobListInfoSummary>> findAroundCarbobInfoSummaryList
+    public ResponseDto<ListResponse<AroundCarbobListInfoSummary>> findAroundCarbobInfoSummaryList
             (Double latitude, Double longitude)
     {
-        List<AroundCarbobListInfoSummary> aroundCarbobList =  aroundCarbobRepositoryImpl.findAroundCarbobByMyPosition(latitude,longitude);
-        return ResponseDto.map(HttpStatus.OK.value(), "반경 5KM 카밥 리스트가 반환되었습니다", aroundCarbobList);
+        List<AroundCarbobListInfoSummary> aroundCarbobList =  aroundCarbobRepositoryImpl.findAroundCarbobSummaryByMyPosition(latitude,longitude);
+        return ResponseDto.map(HttpStatus.OK.value(), "반경 5KM 카밥 리스트가 반환되었습니다", ListResponse.of(aroundCarbobList));
+    }
+
+    public ResponseDto<ListResponse<AroundCarbobListInfo>> findAroundCarbobInfoList
+            (Double latitude, Double longitude, CarbobListStateSortType sortType)
+    {
+        List<AroundCarbobListInfo> aroundCarbobList =  aroundCarbobRepositoryImpl.findAroundCarbobByMyPosition(latitude,longitude,sortType);
+        return ResponseDto.map(HttpStatus.OK.value(), "반경 5KM 카밥 리스트(footer용)가 반환되었습니다", ListResponse.of(aroundCarbobList));
     }
 
 }
---
+++ BE/src/main/java/com/softeer/team6four/domain/reservation/presentation/CarbobListStateSortType.java
@@ -0,0 +1,7 @@
+package com.softeer.team6four.domain.carbob.presentation;
+
+public enum CarbobListStateSortType {
+    DEFAULT_SORT_TYPE, // 정렬 없음
+    SPEED, //속도순으로 정렬
+    PRICE // 가격순으로 정렬
+}
