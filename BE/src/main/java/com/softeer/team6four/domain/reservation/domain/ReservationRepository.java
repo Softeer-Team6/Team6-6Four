b08546ffb2c6eb02a74e15a44150541fab735122
99winnmin <ojysep9987@gmail.com>
2024-02-14 16:06:04 +0900

    :sparkles: feat : 예약 조회 JPQL 추가
    - 예약id로 조회할 시 Carbob 을 fetch join 하여 추가적인 select 가 발생하지않도록함
    
    Related to: #84

--- BE/src/main/java/com/softeer/team6four/domain/reservation/domain/ReservationRepository.java
+++ BE/src/main/java/com/softeer/team6four/domain/reservation/domain/ReservationRepository.java
@@ -3,9 +3,14 @@ package com.softeer.team6four.domain.reservation.domain;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Optional<Reservation> findReservationByCarbob_CarbobIdAndStateType(Long carbobId, StateType stateType);
     List<Reservation> findAllByCarbob_CarbobIdAndStateType(Long carbobId, StateType stateType);
     List<Reservation> findAllByCarbob_CarbobIdAndGuest_UserIdAndStateType(Long carbobId, Long userId, StateType stateType);
+
+    @Query("SELECT r FROM Reservation r JOIN FETCH r.carbob WHERE r.reservationId = :reservationId")
+    Optional<Reservation> findReservationWithCarbobById(@Param("reservationId") Long reservationId);
 }
