782a781f30cbacf4c1994b3d4047926a9c4ebbe3
kimseongwooo <tjddn929@naver.com>
2024-02-14 10:27:11 +0900

    :truck: Feat : 최종 등록 화면 처음 화면으로 버튼 기능 구현 - 최종 등록 화면에서 처음 화면으로 버튼을 클릭하였을 때 홈 화면으로 전환하도록 구현
    Related to: #63

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterCompleteFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/RegisterCompleteFragment.kt
@@ -1,11 +1,11 @@
 package com.softeer.team6four.ui.mypage.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.softeer.team6four.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.softeer.team6four.databinding.FragmentRegisterCompleteBinding
 
 class RegisterCompleteFragment : Fragment() {
@@ -19,6 +19,13 @@ class RegisterCompleteFragment : Fragment() {
     ): View {
         _binding = FragmentRegisterCompleteBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNext.setOnClickListener {
+            requireParentFragment().requireParentFragment().findNavController().popBackStack()
+        }
     }
 
     override fun onDestroy() {
