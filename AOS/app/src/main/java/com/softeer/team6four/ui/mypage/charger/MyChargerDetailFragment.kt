fe9587d3fb11e5391cd5ba45b7d5c3aa5ddca4e2
kimseongwooo <tjddn929@naver.com>
2024-02-14 17:03:58 +0900

    :lipstick: Feat : MyChargerDetailFragment, MyChargerReservationFragment 클래스 생성 - 내 카밥 예약현황 화면을 담당하는 MyChargerReservationFragment 클래스 생성 - 내 카밥 상세설명 화면을 담당하는 MyChargerDetailFragment 클래스 생성 - 바인딩 변수를 통한 프래그먼트 바인딩
    Related to: #83

---
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/charger/MyChargerDetailFragment.kt
@@ -0,0 +1,28 @@
+package com.softeer.team6four.ui.mypage.charger
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.softeer.team6four.databinding.FragmentMyChargerDetailBinding
+
+
+class MyChargerDetailFragment : Fragment() {
+    private var _binding: FragmentMyChargerDetailBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMyChargerDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
