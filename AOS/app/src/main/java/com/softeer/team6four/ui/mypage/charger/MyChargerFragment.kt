c2745f44730df61b4742b472f448e5f95aed1631
kimseongwooo <tjddn929@naver.com>
2024-02-15 00:20:03 +0900

    :lipstick: Feat : 내가 등록한 카밥 아이템을 클릭하였을 때 나타나는 예약 확인, 상세 설명 버튼 클릭 시 화면 전환 기능 구현
    - 내 카밥 리스트에서 아이템을 클릭하였을 때 예약 확인, 상세 설명 탭이 나타나는 데 각 탭을 클릭하였을 때 화면이 전환되도록 화면 전환 기능 구현
    
    Related to: #83

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/charger/MyChargerFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/charger/MyChargerFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.softeer.team6four.R
 import com.softeer.team6four.databinding.FragmentMyChargerBinding
 
 class MyChargerFragment : Fragment() {
@@ -18,6 +21,27 @@ class MyChargerFragment : Fragment() {
     ): View {
         _binding = FragmentMyChargerBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.chipGroupTap.setOnCheckedStateChangeListener { _, checkedIds ->
+            when (checkedIds[0]) {
+                R.id.chip_confirm_reservation -> {
+                    binding.myChargerFragContainer.getFragment<NavHostFragment>()
+                        .findNavController()
+                        .navigate(R.id.action_myChargerDetailFragment_to_myChargerReservationFragment)
+                }
+
+                R.id.chip_detail_description -> {
+                    binding.myChargerFragContainer.getFragment<NavHostFragment>()
+                        .findNavController()
+                        .navigate(R.id.action_myChargerReservationFragment_to_myChargerDetailFragment)
+                }
+
+            }
+        }
     }
 
     override fun onDestroy() {
