b20c7e3aa42efc4c29a785cedadb34be9bfb2e11
yeriinnn <angelrin7@naver.com>
2024-02-14 17:45:48 +0900

    feat: CarbobController 클래스 추가
    - 카밥등록을 위한 내용 추가
    
    Related to: #36

--- BE/src/main/java/com/softeer/team6four/domain/carbob/presentation/CarbobController.java
+++ BE/src/main/java/com/softeer/team6four/domain/carbob/presentation/CarbobController.java
@@ -1,6 +1,9 @@
 package com.softeer.team6four.domain.carbob.presentation;
 
+import com.softeer.team6four.domain.carbob.application.CarbobRegistrationService;
 import com.softeer.team6four.domain.carbob.application.CarbobSearchService;
+import com.softeer.team6four.domain.carbob.application.request.CarbobRegistration;
+import com.softeer.team6four.domain.carbob.application.response.CarbobQr;
 import com.softeer.team6four.domain.carbob.application.response.MyCarbobDetailInfo;
 import com.softeer.team6four.domain.carbob.application.response.MyCarbobSummary;
 import com.softeer.team6four.global.annotation.Auth;
@@ -8,19 +11,19 @@ import com.softeer.team6four.global.filter.UserContextHolder;
 import com.softeer.team6four.global.response.ResponseDto;
 import com.softeer.team6four.global.response.SliceResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/v1/carbob")
 @RequiredArgsConstructor
 public class CarbobController {
     private final CarbobSearchService carbobSearchService;
+    private final CarbobRegistrationService carbobRegistrationService;
 
     @Auth
     @GetMapping(value = "/my")
@@ -41,5 +44,16 @@ public class CarbobController {
     public ResponseDto<MyCarbobDetailInfo> getMyCarbobDetail(@PathVariable Long carbobId) {
         Long userId = UserContextHolder.get();
         return carbobSearchService.findMyCarbobDetailInfo(userId, carbobId);
+    }
+
+    @Auth
+    @PostMapping(value = "/registration")
+    public ResponseDto<CarbobQr> registerCarbob
+            (
+                    @RequestBody CarbobRegistration carbobRegistration
+            )
+    {
+        Long userId = UserContextHolder.get();
+        return carbobRegistrationService.registerCarbob(userId,carbobRegistration);
     }
 }
