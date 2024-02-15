b713fd7179b35722a1c9b3f56aff803e33d31dbf
yeriinnn <angelrin7@naver.com>
2024-02-15 16:42:38 +0900

    feat: S3Config 파일 수정
    - 키 값 변경
    
    Related to: #72

--- BE/src/main/java/com/softeer/team6four/global/config/S3Config.java
+++ BE/src/main/java/com/softeer/team6four/global/config/S3Config.java
@@ -10,13 +10,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${spring.cloud.aws.credentials.accessKey}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${spring.cloud.aws.credentials.secretKey}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${spring.cloud.aws.region.static}")
     private String region;
 
     @Bean
