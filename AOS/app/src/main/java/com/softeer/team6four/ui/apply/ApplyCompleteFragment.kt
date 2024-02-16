3a7a5d6352d3c1adfba85d6550e99357731cd023
kimseongwooo <tjddn929@naver.com>
2024-02-16 17:58:42 +0900

    :lipstick: Feat : ApplyCompleteFragment 클래스 생성 - 신청 완료 화면을 담당하는 ApplyCompleteFragment 클래스 생성 - binding 변수릁 통한 프래그먼트 바인딩
    Related to: #95

---
+++ AOS/app/src/main/java/com/softeer/team6four/ui/apply/ApplyCompleteFragment.kt
@@ -0,0 +1,28 @@
+package com.softeer.team6four.ui.apply
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.softeer.team6four.databinding.FragmentApplyCompleteBinding
+
+class ApplyCompleteFragment : Fragment() {
+    private var _binding: FragmentApplyCompleteBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentApplyCompleteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
