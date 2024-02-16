c83ecd196d14b808679185d3d99641eee0843a9b
kimseongwooo <tjddn929@naver.com>
2024-02-16 17:55:08 +0900

    :lipstick: Feat : ApplyDetailFragment -> ApplyTimeFragment 화면 전환 작업 - nav_graph.xml 파일에 destination으로 ApplyTimeFragment 추가 - ApplyDetailFragment -> ApplyTimeFragment 전환 액션 추가 - ApplyDetailFragment 다음 버튼 클릭 시 화면 전환 기능 구현
    Related to: #95

--- AOS/app/src/main/java/com/softeer/team6four/ui/apply/ApplyDetailFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/apply/ApplyDetailFragment.kt
@@ -1,10 +1,11 @@
 package com.softeer.team6four.ui.apply
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.softeer.team6four.R
 import com.softeer.team6four.databinding.FragmentApplyDetailBinding
 
@@ -16,9 +17,13 @@ class ApplyDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentApplyDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNext.setOnClickListener { findNavController().navigate(R.id.action_applyDetailFragment_to_applyTimeFragment) }
     }
 
     override fun onDestroy() {
