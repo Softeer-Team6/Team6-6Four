1564ed5b69d660c779570583f6ddab7ec46ef825
yeriinnn <angelrin7@naver.com>
2024-02-18 15:41:01 +0900

    feat: CarbobRegistrationService 클래스 내 기능 추가
    - QR 코드 생성 로직 및 S3 업로드 기능 추가
    - QR 코드는 Carbob 객체의 고유 ID를 기반으로 생성
    - MultiFormatWriter를 사용하여 텍스트를 QR 코드로 인코딩하고 BitMatrix를 이용하여 이미지로 변환
      BufferedImage를 사용하여 이미지를 생성하고, ImageIO를 통해 ByteArrayOutputStream에 이미지를 쓴 후 byte 배열로 변환
    - 생성된 QR 이미지는 S3에 업로드 됨
    "QR/" 폴더 내에 UUID로 생성된 파일명과 함께 S3 버킷에 업로드
     업로드된 이미지의 URL은 Carbob 객체의 qrImageUrl 필드에 저장
    - QR 코드의 경우 카밥 등록 시에 자동으로 생성되며, S3에 저장된 이미지 URL을 통해 QR 코드를 확인 가능
    
    Related to: #99

--- BE/src/main/java/com/softeer/team6four/domain/carbob/application/CarbobRegistrationService.java
+++ BE/src/main/java/com/softeer/team6four/domain/carbob/application/CarbobRegistrationService.java
@@ -1,8 +1,32 @@
 package com.softeer.team6four.domain.carbob.application;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.softeer.team6four.domain.carbob.application.exception.CarbobException;
 import com.softeer.team6four.domain.carbob.application.request.CarbobRegistration;
 import com.softeer.team6four.domain.carbob.application.response.CarbobQr;
-import com.softeer.team6four.domain.carbob.domain.*;
+import com.softeer.team6four.domain.carbob.domain.Carbob;
+import com.softeer.team6four.domain.carbob.domain.CarbobInfo;
+import com.softeer.team6four.domain.carbob.domain.CarbobLocation;
+import com.softeer.team6four.domain.carbob.domain.CarbobSpec;
+import com.softeer.team6four.domain.carbob.domain.InstallType;
+import com.softeer.team6four.domain.carbob.domain.LocationType;
+import com.softeer.team6four.domain.carbob.domain.ChargerType;
+import com.softeer.team6four.domain.carbob.domain.SpeedType;
+import com.softeer.team6four.domain.carbob.domain.CarbobRepository;
 import com.softeer.team6four.domain.reservation.application.ReservationMapper;
 import com.softeer.team6four.domain.reservation.domain.Reservation;
 import com.softeer.team6four.domain.reservation.domain.ReservationLine;
@@ -12,16 +36,16 @@ import com.softeer.team6four.domain.user.domain.User;
 import com.softeer.team6four.domain.user.domain.UserRepository;
 import com.softeer.team6four.global.response.ErrorCode;
 import com.softeer.team6four.global.response.ResponseDto;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
+import javax.imageio.ImageIO;
 
 @Slf4j
 @Service
@@ -30,12 +54,15 @@ public class CarbobRegistrationService {
     private final UserRepository userRepository;
     private final CarbobRepository carbobRepository;
     private final ReservationRepository reservationRepository;
+    private final AmazonS3 amazonS3;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
 
     @Transactional
     public ResponseDto<CarbobQr> registerCarbob(Long userId, CarbobRegistration carbobRegistration) {
         User host = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND));
-
 
         CarbobLocation location = CarbobLocation.builder()
                 .address(carbobRegistration.getAddress())
@@ -49,7 +76,6 @@ public class CarbobRegistrationService {
                 .speedType(SpeedType.valueOf(carbobRegistration.getSpeedType()))
                 .installType(InstallType.valueOf(carbobRegistration.getInstallType()))
                 .build();
-
 
         CarbobInfo carbobInfo = CarbobInfo.builder()
                 .description(carbobRegistration.getDescription())
@@ -65,14 +91,70 @@ public class CarbobRegistrationService {
                 .host(host)
                 .build();
 
-        carbobRepository.save(carbob);
+        try {
+            // QR 코드 생성 및 S3에 업로드
+            String qrImageUrl = generateQRCode(carbob);
+            carbob.setCarbobQrImageUrl(qrImageUrl);
 
-        CarbobQr carbobQr = new CarbobQr(carbob.getQrImageUrl());
+            // 카밥 저장
+            carbobRepository.save(carbob);
 
-        List<ReservationLine> newReservationLines = makeReservationLines(carbobRegistration);
+            // 카밥 QR 코드 URL 생성
+            CarbobQr carbobQr = new CarbobQr(carbob.getCarbobQrImageUrl());
 
-        Reservation savedReservation = reservationRepository.save(ReservationMapper.mapToSelfReservationEntity(carbob,host,newReservationLines));
-        return ResponseDto.map(HttpStatus.OK.value(), "카밥 등록에 성공했습니다.", carbobQr);
+            List<ReservationLine> newReservationLines = makeReservationLines(carbobRegistration);
+
+            Reservation savedReservation = reservationRepository.save(ReservationMapper.mapToSelfReservationEntity(carbob, host, newReservationLines));
+
+            return ResponseDto.map(HttpStatus.OK.value(), "카밥 등록에 성공했습니다.", carbobQr);
+        } catch (IOException e) {
+            throw new CarbobException(ErrorCode.CARBOB_REGISTRATION_FAILED);
+        }
+    }
+
+    // QR 코드 생성
+    private String generateQRCode(Carbob carbob) throws IOException {
+        try {
+            Long carbobId = carbob.getCarbobId(); // 카밥 객체로부터 carbobId를 가져옴
+            String text = String.valueOf(carbobId);
+
+            int width = 300;
+            int height = 300;
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            // QR 코드 생성
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height);
+
+            // 이미지로 변환
+            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bufferedImage.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+                }
+            }
+
+            // ByteArrayOutputStream에 이미지 쓰기
+            ImageIO.write(bufferedImage, "png", baos);
+
+            // byte 배열로 변환
+            byte[] bytes = baos.toByteArray();
+
+            // S3에 업로드
+            String fileName = "QR/" + UUID.randomUUID().toString() + ".png"; // QR 폴더 내에 이미지 저장
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(bytes.length);
+            metadata.setContentType("image/png");
+
+            amazonS3.putObject(bucket, fileName, new ByteArrayInputStream(bytes), metadata);
+
+            // 업로드된 이미지의 URL 반환
+            URL url = amazonS3.getUrl(bucket, fileName);
+            return url.toString();
+        } catch (Exception e) {
+            log.error("QR 코드 생성 또는 S3 업로드 중 오류가 발생했습니다.", e);
+            throw new CarbobException(ErrorCode.CARBOB_QR_FAILED);
+        }
     }
 
     private List<ReservationLine> makeReservationLines(CarbobRegistration carbobRegistration) {
@@ -87,4 +169,3 @@ public class CarbobRegistrationService {
         return reservationLines;
     }
 }
-
