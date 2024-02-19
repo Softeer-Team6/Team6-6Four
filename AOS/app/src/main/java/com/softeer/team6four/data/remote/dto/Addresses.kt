af67317e16c5c7683ccf933b51c9619ce26fd2a2
kimseongwooo <tjddn929@naver.com>
2024-02-19 09:51:49 +0900

    :sparkles: Feat : Dto 클래스 파일 추가 - 네이버 GeoCode API 통신을 위한 Dto 클래스 파일 추가
    Related to: #103

---
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/dto/Addresses.kt
@@ -0,0 +1,16 @@
+package com.softeer.team6four.data.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+@SerialName("addresses")
+data class Addresses(
+    val addressElements: List<AddressElement>,
+    val distance: Double,
+    val englishAddress: String,
+    val jibunAddress: String,
+    val roadAddress: String,
+    val x: String,
+    val y: String
+)
