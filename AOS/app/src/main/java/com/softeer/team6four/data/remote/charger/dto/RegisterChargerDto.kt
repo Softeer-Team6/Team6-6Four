64ece701d996d47f63571b856fefc61a9e88d67a
kimseongwooo <tjddn929@naver.com>
2024-02-19 22:37:32 +0900

    :sparkles: Feat : RegisterChargerDto 클래스 생성 - Charger 도메인의 충전소(카밥) 등록 API 관련 dto 클래스 생성
    Related to: #115

---
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/charger/dto/RegisterChargerDto.kt
@@ -0,0 +1,8 @@
+package com.softeer.team6four.data.remote.charger.dto
+
+data class RegisterChargerDto(
+    val data: Any?,
+    val message: String,
+    val statusCode: Int,
+    val timestamp: String
+)
