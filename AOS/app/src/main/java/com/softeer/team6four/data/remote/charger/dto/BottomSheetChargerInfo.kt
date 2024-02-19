cded1f3d64cdf5aed8c7d070fe7c72e4de69f057
kimseongwooo <tjddn929@naver.com>
2024-02-19 22:29:05 +0900

    :sparkles: Feat : BottomSheetChargerListDto, BottomSheetChargerList, BottomSheetChargerInfo 클래스 생성 - Charger 도메인의 내 위치 근처 충전소 API 관련 dto 클래스 생성
    Related to: #115

---
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/charger/dto/BottomSheetChargerInfo.kt
@@ -0,0 +1,9 @@
+package com.softeer.team6four.data.remote.charger.dto
+
+data class BottomSheetChargerInfo(
+    val address: String,
+    val carbobId: Int,
+    val feePerHour: String,
+    val nickname: String,
+    val speedType: String
+)
---
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/charger/dto/BottomSheetChargerList.kt
@@ -0,0 +1,6 @@
+package com.softeer.team6four.data.remote.charger.dto
+
+data class BottomSheetChargerList(
+    val content: List<BottomSheetChargerInfo>,
+    val size: Int
+)
