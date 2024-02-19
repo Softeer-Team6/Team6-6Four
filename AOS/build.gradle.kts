8248025b52b28d57d43c949e834deec853411ce9
kimseongwooo <tjddn929@naver.com>
2024-02-19 16:21:54 +0900

    :arrow_up: Setting : hilt 버전 업그레이드 - hilt 버전 업그레이드
    Related to: #103

--- AOS/build.gradle.kts
+++ AOS/build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     kotlin("jvm") version "1.9.21"
     kotlin("plugin.serialization") version "1.9.21"
