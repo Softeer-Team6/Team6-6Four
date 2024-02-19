c40738011f4430e64363d0709b85534b37ec2c2d
yeriinnn <angelrin7@naver.com>
2024-02-19 11:37:15 +0900

    refactor: CarbobController 수정
    - return 값 변경
    
    Related to: #99

--- BE/src/main/java/com/softeer/team6four/domain/carbob/presentation/CarbobController.java
+++ BE/src/main/java/com/softeer/team6four/domain/carbob/presentation/CarbobController.java
@@ -2,7 +2,6 @@ package com.softeer.team6four.domain.carbob.presentation;
 
 import com.softeer.team6four.domain.carbob.application.*;
 import com.softeer.team6four.domain.carbob.application.request.CarbobRegistration;
-import com.softeer.team6four.domain.carbob.application.response.CarbobQr;
 import com.softeer.team6four.domain.carbob.application.response.MyCarbobDetailInfo;
 import com.softeer.team6four.domain.carbob.application.response.MyCarbobSummary;
 import com.google.firebase.database.annotations.NotNull;
@@ -10,6 +9,7 @@ import com.softeer.team6four.domain.carbob.application.CarbobSearchService;
 import com.softeer.team6four.domain.carbob.application.response.*;
 import com.softeer.team6four.global.annotation.Auth;
 import com.softeer.team6four.global.filter.UserContextHolder;
+import com.softeer.team6four.global.infrastructure.s3.S3Service;
 import com.softeer.team6four.global.response.ListResponse;
 import com.softeer.team6four.global.response.ResponseDto;
 import com.softeer.team6four.global.response.SliceResponse;
@@ -17,10 +17,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -30,7 +30,7 @@ public class CarbobController {
     private final CarbobSearchService carbobSearchService;
     private final CarbobRegistrationService carbobRegistrationService;
     private final AroundCarbobSearchService aroundCarbobSearchService;
-    private final CarbobImgUploadService carbobImgUploadService;
+    private final S3Service s3Service;
 
     @Auth
     @GetMapping(value = "/my")
@@ -89,13 +89,13 @@ public class CarbobController {
 
     @Auth
     @PostMapping(value = "/registration")
-    public ResponseDto<CarbobQr> registerCarbob
+    public ResponseDto<Void> registerCarbob
             (
                     @RequestBody CarbobRegistration carbobRegistration
             )
     {
         Long userId = UserContextHolder.get();
-        return carbobRegistrationService.registerCarbob(userId,carbobRegistration);
+        return carbobRegistrationService.registerCarbob(userId, carbobRegistration);
     }
 
     @Auth
@@ -105,7 +105,11 @@ public class CarbobController {
                     @RequestPart MultipartFile image
             )
     {
-        return carbobImgUploadService.saveFile(image);
+        String imageUrl = s3Service.saveCarbobAndReturnImageUrl(image);
+        CarbobImgUrl carbobImgUrl = CarbobImgUrl.builder()
+                .imgUrl(imageUrl)
+                .build();
+        return ResponseDto.map(HttpStatus.OK.value(), "S3에 이미지 등록 후 링크 반환이 성공했습니다.", carbobImgUrl);
     }
 
 }
