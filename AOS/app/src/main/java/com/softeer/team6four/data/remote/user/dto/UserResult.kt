02a85dd921afb9b640a3976996bc94968d516a4b
kimseongwooo <tjddn929@naver.com>
2024-02-21 01:50:56 +0900

    :art: Fix : UserResult 클래스 수정 - Serializable 어노테이션 추가
    Related to: #123

--- AOS/app/src/main/java/com/softeer/team6four/data/remote/user/dto/UserResult.kt
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/user/dto/UserResult.kt
@@ -1,7 +1,10 @@
 package com.softeer.team6four.data.remote.user.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class UserResult(
     val accessToken: String,
     val refreshToken: String,
-    val nickname: String
+//    val nickname: String
 )
