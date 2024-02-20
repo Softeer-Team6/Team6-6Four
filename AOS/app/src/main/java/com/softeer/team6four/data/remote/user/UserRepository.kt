a4ad85f4df2919c3d5552eeb7e75ce5c0d688558
kimseongwooo <tjddn929@naver.com>
2024-02-21 00:05:01 +0900

    :sparkles: Feat : UserRepository 클래스 생성 - UserDataSource 클래스에서 가져온 api 데이터를 처리하기 위한 UserRepository 클래스 생성
    Related to: #123

---
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/user/UserRepository.kt
@@ -0,0 +1,65 @@
+package com.softeer.team6four.data.remote.user
+
+import com.softeer.team6four.data.Resource
+import com.softeer.team6four.data.remote.user.dto.SignUpInfo
+import com.softeer.team6four.data.remote.user.model.EmailCheckModel
+import com.softeer.team6four.data.remote.user.model.NicknameCheckModel
+import com.softeer.team6four.data.remote.user.model.UserInfoModel
+import com.softeer.team6four.data.remote.user.source.UserDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepository @Inject constructor(private val userDataSource: UserDataSource) {
+    fun fetchEmailCheck(email: String): Flow<Resource<EmailCheckModel>> {
+        return userDataSource.checkEmail(email).map { resource ->
+            when (resource) {
+                is Resource.Success -> {
+                    val emailExistsResult = resource.data
+                    val emailCheckModel = EmailCheckModel(emailExistsResult.emailExists)
+                    Resource.Success(emailCheckModel)
+                }
+
+                is Resource.Error -> {
+                    Resource.Error(message = resource.message)
+                }
+
+                else -> {
+                    Resource.Loading()
+                }
+            }
+        }
+    }
+
+    fun fetchNicknameCheck(nickname: String): Flow<Resource<NicknameCheckModel>> {
+        return userDataSource.checkNickname(nickname).map { resource ->
+            when (resource) {
+                is Resource.Success -> {
+                    val nicknameExistsResult = resource.data
+                    val emailCheckModel = NicknameCheckModel(nicknameExistsResult.nicknameExists)
+                    Resource.Success(emailCheckModel)
+                }
+
+                is Resource.Error -> {
+                    Resource.Error(message = resource.message)
+                }
+
+                else -> {
+                    Resource.Loading()
+                }
+            }
+        }
+    }
+
+    fun requestSignUp(userInfoModel: UserInfoModel): Flow<Resource<Any>> {
+        val singUpInfo = SignUpInfo(
+            email = userInfoModel.email,
+            password = userInfoModel.password,
+            nickname = userInfoModel.nickname
+        )
+        return userDataSource.requestSignUp(singUpInfo)
+    }
+
+}
