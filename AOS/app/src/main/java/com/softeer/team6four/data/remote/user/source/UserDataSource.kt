feae86b73512150ed9a0146aa89b898603669d07
kimseongwooo <tjddn929@naver.com>
2024-02-21 01:57:30 +0900

    :sparkles: Feat : UserDataSource 로그인 요청 api 관련 함수 정의 - UserDataSource 로그인 요청 api 관련 함수 정의
    Related to: #123

--- AOS/app/src/main/java/com/softeer/team6four/data/remote/user/source/UserDataSource.kt
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/user/source/UserDataSource.kt
@@ -3,8 +3,10 @@ package com.softeer.team6four.data.remote.user.source
 import com.softeer.team6four.api.UserService
 import com.softeer.team6four.data.Resource
 import com.softeer.team6four.data.remote.user.dto.EmailExistsResult
+import com.softeer.team6four.data.remote.user.dto.LoginInfo
 import com.softeer.team6four.data.remote.user.dto.NicknameExistsResult
 import com.softeer.team6four.data.remote.user.dto.SignUpInfo
+import com.softeer.team6four.data.remote.user.dto.UserResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -68,6 +70,25 @@ class UserDataSource @Inject constructor(private val userService: UserService) {
             }
         } catch (e: Exception) {
             emit(Resource.Error(message = "Failed to Sign Up"))
+        }
+    }
+
+    fun requestLogin(nickname: String, password: String): Flow<Resource<UserResult>> = flow {
+        emit(Resource.Loading())
+        try {
+            val loginResponse = userService.requestLogin(LoginInfo( nickname, password))
+            if (loginResponse.isSuccessful) {
+                val userResult = loginResponse.body()?.data
+                emit(
+                    Resource.Success(
+                        userResult ?: throw Exception("Response body is null")
+                    )
+                )
+            } else {
+                emit(Resource.Error(loginResponse.code(), "Failed to Login"))
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error(message = e.message.toString()))
         }
     }
 }
