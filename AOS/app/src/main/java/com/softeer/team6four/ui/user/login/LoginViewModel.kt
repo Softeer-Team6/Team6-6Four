3e15d27a2dca5167a37823662192474650fd9961
kimseongwooo <tjddn929@naver.com>
2024-02-21 00:16:16 +0900

    :sparkles: Feat : LoginViewModel 클래스 생성 - 로그인 화면 UI 상태 관리를 위한 뷰모델 클래스 생성
    Related to: #123

---
+++ AOS/app/src/main/java/com/softeer/team6four/ui/user/login/LoginViewModel.kt
@@ -0,0 +1,10 @@
+package com.softeer.team6four.ui.user.login
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor() : ViewModel() {
+
+}
