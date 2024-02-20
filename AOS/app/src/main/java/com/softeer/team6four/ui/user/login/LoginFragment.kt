9748d75c231db200b8412b3bf2740ec3e011c8e5
kimseongwooo <tjddn929@naver.com>
2024-02-21 02:10:35 +0900

    :sparkles: Feat : LoginFragment 클래스 데이터 바인딩 작업 - 데이터 바인딩을 위해 뷰모델과 lifecycleowner 할당 - 로그인 성공 시 화면 전환 기능 구현
    Related to: #123

--- AOS/app/src/main/java/com/softeer/team6four/ui/user/login/LoginFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/user/login/LoginFragment.kt
@@ -1,17 +1,24 @@
 package com.softeer.team6four.ui.user.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.softeer.team6four.R
 import com.softeer.team6four.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
+    private val loginViewModel: LoginViewModel by viewModels()
     private val binding
         get() = _binding!!
 
@@ -26,7 +33,17 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
-            btnLogin.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_homeFragment) }
+            viewModel = loginViewModel
+            lifecycleOwner = viewLifecycleOwner
+
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    loginViewModel.loginSuccessState.collect { result ->
+                        if (result) findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                    }
+                }
+            }
+
             tvSignup.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_signUpFragment) }
         }
     }
