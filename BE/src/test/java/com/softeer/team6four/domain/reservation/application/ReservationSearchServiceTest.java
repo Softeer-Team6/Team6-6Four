fe097a52adbd90d94898af1119dda72b1ff3a1ef
DSL2e <kdb2013@g.skku.edu>
2024-02-20 20:05:38 +0900

    :white_check_mark: test: ReservationSearchService 테스트코드 추가
    - 특정 카밥에 대해 특정 날짜에서 예약 가능/불가능 시간 조회 테스트 추가
    
    Related to : #126

--- BE/src/test/java/com/softeer/team6four/domain/reservation/application/ReservationSearchServiceTest.java
+++ BE/src/test/java/com/softeer/team6four/domain/reservation/application/ReservationSearchServiceTest.java
@@ -3,8 +3,11 @@ package com.softeer.team6four.domain.reservation.application;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +17,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.softeer.team6four.domain.reservation.application.response.DailyReservationInfo;
 import com.softeer.team6four.domain.reservation.domain.Reservation;
 import com.softeer.team6four.domain.reservation.domain.ReservationLine;
 import com.softeer.team6four.domain.reservation.domain.ReservationRepository;
@@ -88,7 +92,38 @@ class ReservationSearchServiceTest {
 	}
 
 	@Test
+	@DisplayName("특정 카밥 예약 불가능/가능 시간 조회")
 	void getDailyReservationStatus() {
+		// Given
+		Long carbobId = 123L;
+		String date = "2024-02-20";
+		LocalDate localDate = LocalDate.parse(date);
+		LocalDateTime startDateTime = localDate.atStartOfDay();
+		LocalDateTime endDateTime = localDate.plusDays(1).atStartOfDay().minusSeconds(1);
+		boolean[] expectedUnavailableTimes = new boolean[24];
+
+		List<LocalDateTime> reservationTimes = new ArrayList<>();
+		reservationTimes.add(LocalDateTime.of(localDate, LocalDateTime.now().toLocalTime()));
+		reservationTimes.add(LocalDateTime.of(localDate, LocalDateTime.now().plusHours(1).toLocalTime()));
+		reservationTimes.add(LocalDateTime.of(localDate, LocalDateTime.now().plusHours(2).toLocalTime()));
+		reservationTimes.add(LocalDateTime.of(localDate, LocalDateTime.now().plusHours(3).toLocalTime()));
+
+		when(dailyReservationRepositoryImpl.findDailyReservationStatus(carbobId, startDateTime, endDateTime))
+			.thenReturn(reservationTimes);
+
+		// When
+		DailyReservationInfo dailyReservationInfo = reservationSearchService.getDailyReservationStatus(carbobId, date);
+
+		// Then
+		assertEquals(expectedUnavailableTimes.length, dailyReservationInfo.getDailyBookedTimeCheck().length);
+		// 예약이 있는 시간에 대한 배열 요소가 true로 설정되어 있는지 확인
+		for (LocalDateTime reservationTime : reservationTimes) {
+			int hour = reservationTime.getHour();
+			assertTrue(dailyReservationInfo.getDailyBookedTimeCheck()[hour]);
+		}
+		// repository 메서드가 호출되었는지 확인
+		verify(dailyReservationRepositoryImpl, times(1)).findDailyReservationStatus(carbobId, startDateTime, endDateTime);
+
 	}
 
 }
