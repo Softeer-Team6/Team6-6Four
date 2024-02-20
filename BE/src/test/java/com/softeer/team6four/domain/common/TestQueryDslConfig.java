10f850a34b374ca8026f947c6b43f6e4af178c88
DSL2e <kdb2013@g.skku.edu>
2024-02-20 19:43:03 +0900

    :white_check_mark: test: DailyReservationRepositoryImpl 테스트코드 추가
    - DB에 더미데이터를 넣어두고 원하는 결과 값이 나오는지 테스트
    
    Related to : #126

--- BE/src/test/java/com/softeer/team6four/domain/common/TestQueryDslConfig.java
+++ BE/src/test/java/com/softeer/team6four/domain/common/TestQueryDslConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.softeer.team6four.domain.carbob.infra.CarbobRepositoryImpl;
 import com.softeer.team6four.domain.payment.infra.PaymentRepositoryImpl;
+import com.softeer.team6four.domain.reservation.infra.DailyReservationRepositoryImpl;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -30,5 +31,9 @@ public class TestQueryDslConfig {
 	@Bean
 	public PaymentRepositoryImpl paymentRepositoryImpl() {
 		return new PaymentRepositoryImpl(jpaQueryFactory());
+	}
+	@Bean
+	public DailyReservationRepositoryImpl DailyReservationRepositoryImpl() {
+		return new DailyReservationRepositoryImpl(jpaQueryFactory());
 	}
 }
