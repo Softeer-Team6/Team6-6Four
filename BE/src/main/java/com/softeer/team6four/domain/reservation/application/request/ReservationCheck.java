8fbca01fec4a5d69666bf799095c21d9685c92b7
DSL2e <kdb2013@g.skku.edu>
2024-02-20 17:41:44 +0900

    :art: fix: 빌더 추가
    - 테스트코드를 작성을 위해 빌더 추가
    
    Related to : #126

--- BE/src/main/java/com/softeer/team6four/domain/reservation/application/request/ReservationCheck.java
+++ BE/src/main/java/com/softeer/team6four/domain/reservation/application/request/ReservationCheck.java
@@ -2,9 +2,11 @@ package com.softeer.team6four.domain.reservation.application.request;
 
 import com.softeer.team6four.domain.reservation.domain.StateType;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ReservationCheck {
 	private StateType stateType;
 }
