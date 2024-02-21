c247da6a2fdbaf45ccce0a56021e75cde20c229e
99winnmin <ojysep9987@gmail.com>
2024-02-22 01:11:49 +0900

    :sparkles: feat : 내 포인트 화면 데이터 바인딩 및 상태 관찰 로직 추가
    - MyPointViewModel의 fetchMyTotalPoint 메소드를 호출하여 사용자의 총 포인트 정보를 가져옴
    - binding 객체를 사용하여 뷰 모델과 생명주기 소유자를 데이터 바인딩에 설정.
    - lifecycleScope와 repeatOnLifecycle를 사용하여 STARTED 상태일 때만 사용자의 총 포인트를 관찰하고, 포인트 값이 업데이트될 때마다 텍스트 뷰(tvTotalPointsValue)를 갱신.
    
    Related to: #138

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/point/MyPointFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/point/MyPointFragment.kt
@@ -1,14 +1,22 @@
 package com.softeer.team6four.ui.mypage.point
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.softeer.team6four.databinding.FragmentMyPointBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MyPointFragment : Fragment() {
     private var _binding: FragmentMyPointBinding? = null
+    private val myPointViewModel: MyPointViewModel by viewModels()
     private val binding
         get() = _binding!!
 
@@ -19,6 +27,23 @@ class MyPointFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentMyPointBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            myPointViewModel.fetchMyTotalPoint()
+            viewModel = myPointViewModel
+            lifecycleOwner = viewLifecycleOwner
+
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    myPointViewModel.myTotalPoint.collect {
+                        tvTotalPointsValue.text = it.toString()
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
