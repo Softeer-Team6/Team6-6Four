ac8a9ed6c2d63ad18c2d11f40da0d6f75b5c819f
99winnmin <ojysep9987@gmail.com>
2024-02-22 03:30:21 +0900

    :sparkles: feat : 포인트 내역 리스트 조회, 충전 API 연결
    Related to: #138

--- AOS/app/src/main/java/com/softeer/team6four/data/remote/payment/source/PaymentDataSource.kt
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/payment/source/PaymentDataSource.kt
@@ -3,7 +3,9 @@ package com.softeer.team6four.data.remote.payment.source
 import android.util.Log
 import com.softeer.team6four.api.PaymentService
 import com.softeer.team6four.data.Resource
+import com.softeer.team6four.data.remote.payment.dto.ChargePoint
 import com.softeer.team6four.data.remote.payment.dto.Point
+import com.softeer.team6four.data.remote.payment.dto.PointDetailList
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -17,7 +19,6 @@ class PaymentDataSource @Inject constructor(private val paymentService: PaymentS
         val authorization = "Bearer $accessToken"
         try {
             val totalPointResultResponse = paymentService.getTotalPoint3(authorization)
-            Log.e("PaymentDataSource", "getMyTotalPoint: $totalPointResultResponse")
             if (totalPointResultResponse.isSuccessful) {
                 val totalPoint = totalPointResultResponse.body()?.data
                 emit(
@@ -29,9 +30,48 @@ class PaymentDataSource @Inject constructor(private val paymentService: PaymentS
                 emit(Resource.Error(totalPointResultResponse.code(), totalPointResultResponse.message()))
             }
         } catch (e: Exception) {
+            emit(Resource.Error(message = e.message.toString()))
+        }
+    }
+
+    fun getPointHistory(accessToken: String, lastPaymentId: Long?) : Flow<Resource<PointDetailList>> = flow {
+        emit(Resource.Loading())
+        val authorization = "Bearer $accessToken"
+        try {
+            val pointHistoryResultResponse = paymentService.getPointHistory(authorization, lastPaymentId)
+            if (pointHistoryResultResponse.isSuccessful) {
+                val pointHistory = pointHistoryResultResponse.body()?.data
+                emit(
+                    Resource.Success(
+                        pointHistory ?: throw Exception("Response body is null")
+                    )
+                )
+            } else {
+                emit(Resource.Error(pointHistoryResultResponse.code(), pointHistoryResultResponse.message()))
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error(message = e.message.toString()))
+        }
+    }
+
+    fun chargePoint(accessToken: String, amount: Int) : Flow<Resource<Unit>> = flow {
+        emit(Resource.Loading())
+        val authorization = "Bearer $accessToken"
+        try {
+            val chargePointResultResponse = paymentService.requestChargePoint(authorization, ChargePoint(amount))
+            if (chargePointResultResponse.isSuccessful) {
+                emit(Resource.Success(Unit))
+            } else {
+                emit(
+                    Resource.Error(
+                        chargePointResultResponse.code(),
+                        chargePointResultResponse.message()
+                    )
+                )
+            }
+        } catch (e: Exception) {
             Log.e("test", e.stackTraceToString())
             emit(Resource.Error(message = e.message.toString()))
         }
-
     }
 }
