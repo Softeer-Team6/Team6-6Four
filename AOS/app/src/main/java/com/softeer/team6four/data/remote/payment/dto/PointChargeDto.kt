049dbf210d3d1004c88eeda07fb258358aabb1d6
99winnmin <ojysep9987@gmail.com>
2024-02-22 03:28:52 +0900

    :sparkles: feat : 포인트 내역 리스트 조회 및 충전하기 관련 DTO @Serializable 선언 및 오타 수정
    Related to: #138

--- AOS/app/src/main/java/com/softeer/team6four/data/remote/payment/dto/PointChargeDto.kt
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/payment/dto/PointChargeDto.kt
@@ -1,7 +1,10 @@
 package com.softeer.team6four.data.remote.payment.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class PointChargeDto(
-    val data: Point,
+    val data: Unit,
     val message: String,
     val statusCode: Int,
     val timestamp: String
