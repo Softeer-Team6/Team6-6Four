a542186ac3ef66e4df2b9ea9298b4ff2a22bad31
99winnmin <ojysep9987@gmail.com>
2024-02-22 03:32:06 +0900

    :sparkles: feat : 포인트 내역 리스트 조회, 충전 ViewModel 메서드 구현
    - 포인트 충전 성공 시, 총 포인트 조회 및 내역 조회를 다시 하도록함
    
    Related to: #138

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/point/MyPointViewModel.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/point/MyPointViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.softeer.team6four.data.Resource
 import com.softeer.team6four.data.local.UserPreferencesRepository
 import com.softeer.team6four.data.remote.payment.PaymentRepository
+import com.softeer.team6four.data.remote.payment.model.PointHistoryDetailModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -21,6 +22,9 @@ class MyPointViewModel @Inject constructor(
 ) : ViewModel() {
     private val _myTotalPoint: MutableStateFlow<Int> = MutableStateFlow(0)
     val myTotalPoint: StateFlow<Int> = _myTotalPoint
+
+    private val _pointHistory: MutableStateFlow<List<PointHistoryDetailModel>> = MutableStateFlow(emptyList())
+    val pointHistory: StateFlow<List<PointHistoryDetailModel>> = _pointHistory
 
     fun fetchMyTotalPoint() {
         viewModelScope.launch {
@@ -45,6 +49,57 @@ class MyPointViewModel @Inject constructor(
 
                     else -> {
                         _myTotalPoint.value = 0
+                    }
+                }
+            }
+        }
+    }
+
+    fun fetchPointHistory(lastPaymentId: Long? = null) {
+        viewModelScope.launch {
+            val accessToken = userPreferencesRepository.getAccessToken().first()
+            val pointHistoryData = paymentRepository.getPointHistory(accessToken, lastPaymentId)
+
+            pointHistoryData.catch {
+                Log.e("MyPointViewModel", "getPointHistory: $this")
+            }.collect { pointHistory ->
+                when (pointHistory) {
+                    is Resource.Success -> {
+                        _pointHistory.value = pointHistory.data.content
+                    }
+
+                    is Resource.Error -> {
+                        Log.e("MyPointViewModel", "getPointHistory: ${pointHistory.message}")
+                    }
+
+                    else -> {
+                        Log.e("MyPointViewModel", "getPointHistory: $pointHistory")
+                    }
+                }
+            }
+        }
+    }
+
+    fun chargePoint() {
+        viewModelScope.launch {
+            val accessToken = userPreferencesRepository.getAccessToken().first()
+            val chargePointData = paymentRepository.chargePoint(accessToken, 10000)
+
+            chargePointData.catch {
+                Log.e("MyPointViewModel", "chargePoint: $this")
+            }.collect { chargePoint ->
+                when (chargePoint) {
+                    is Resource.Success -> {
+                        fetchMyTotalPoint()
+                        fetchPointHistory()
+                    }
+
+                    is Resource.Error -> {
+                        Log.e("MyPointViewModel", "chargePoint: ${chargePoint.message}")
+                    }
+
+                    else -> {
+                        Log.e("MyPointViewModel", "chargePoint: $chargePoint")
                     }
                 }
             }
