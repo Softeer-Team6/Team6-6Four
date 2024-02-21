0804f7808d2ecf59bb44874f490789aca1bb2379
kimseongwooo <tjddn929@naver.com>
2024-02-21 09:45:22 +0900

    :sparkles: Feat : LoginViewModel에서 로그인 요청 시 토큰 정보 저장 - LoginViewModel 클래스 내 requestLogin 함수에서 token 정보를 저장할 수 있도로 추가
    Related to: #123

--- AOS/app/src/main/java/com/softeer/team6four/ui/user/login/LoginViewModel.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/user/login/LoginViewModel.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.softeer.team6four.data.Resource
+import com.softeer.team6four.data.local.UserPreferencesRepository
 import com.softeer.team6four.data.remote.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,7 +14,10 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val userRepository: UserRepository) : ViewModel() {
+class LoginViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+    private val userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
 
     private val _email: MutableStateFlow<String> = MutableStateFlow("")
     val email: StateFlow<String> = _email
@@ -41,7 +45,14 @@ class LoginViewModel @Inject constructor(private val userRepository: UserReposit
                     is Resource.Success -> {
                         _loginSuccessState.value = true
                         _loginFailState.value = false
+                        with(userPreferencesRepository) {
+                            //updateNickname(userLoginModel.data.nickname)
+                            updateAccessToken(userLoginModel.data.accessToken)
+                            updateRefreshToken(userLoginModel.data.refreshToken)
+                        }
+
                         Log.d("userLoginModel", userLoginModel.data.toString())
+
                     }
 
                     is Resource.Error -> {
