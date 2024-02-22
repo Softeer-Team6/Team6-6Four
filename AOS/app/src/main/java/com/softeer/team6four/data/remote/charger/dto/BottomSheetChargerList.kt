e11793a5b84c4c3dd4311704683619d3e87eccc2
kimseongwooo <tjddn929@naver.com>
2024-02-22 15:28:04 +0900

    :sparkles: Feat : Serializable 어노테이션 추가 - api 통신을 위한 serializable 어노테이션 추가
    Related to: #137

--- AOS/app/src/main/java/com/softeer/team6four/data/remote/charger/dto/BottomSheetChargerList.kt
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/charger/dto/BottomSheetChargerList.kt
@@ -1,5 +1,8 @@
 package com.softeer.team6four.data.remote.charger.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class BottomSheetChargerList(
     val content: List<BottomSheetChargerInfo>,
     val size: Int
