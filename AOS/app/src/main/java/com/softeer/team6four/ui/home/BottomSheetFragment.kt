10588b256e6bd543cf7b16ae089a2696e3257fd8
kimseongwooo <tjddn929@naver.com>
2024-02-22 21:18:49 +0900

    :sparkles: Feat : BottomSheetFragment 클래스 내 어댑터 기능 구현 - 람다를 통해 id를 viewmodel에 정의하고 navigate 함수 호출
    Related to: #153

--- AOS/app/src/main/java/com/softeer/team6four/ui/home/BottomSheetFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/home/BottomSheetFragment.kt
@@ -10,10 +10,12 @@ import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.softeer.team6four.R
 import com.softeer.team6four.databinding.BottomSheetDialogBinding
+import com.softeer.team6four.ui.apply.ApplyViewModel
 
 class BottomSheetFragment : BottomSheetDialogFragment() {
     private var _binding: BottomSheetDialogBinding? = null
     private val homeViewModel: HomeViewModel by activityViewModels()
+    private val applyViewModel: ApplyViewModel by activityViewModels()
     private val binding
         get() = _binding!!
 
@@ -36,7 +38,14 @@ class BottomSheetFragment : BottomSheetDialogFragment() {
                 requireParentFragment().findNavController()
                     .navigate(R.id.action_homeFragment_to_applyDetailFragment)
             }
-            rvBottomSheetChargerList.adapter = BottomSheetListAdapter()
+            rvBottomSheetChargerList.adapter =
+                BottomSheetListAdapter { id ->
+                    homeViewModel.updateSelectedCharger(id)
+                    applyViewModel.updateSelectedChargerId(id)
+                    homeViewModel.updateSelectedCharger()
+                    dismiss()
+                    findNavController().navigate(R.id.action_homeFragment_to_applyDetailFragment)
+                }
 
             orderChipGroup.setOnCheckedStateChangeListener { _, checkedIds ->
                 Log.d("checkedIds.size", checkedIds.size.toString())
