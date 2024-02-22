a08506e47fa883d15e944b9523bfca6440592569
kimseongwooo <tjddn929@naver.com>
2024-02-23 02:38:08 +0900

    :art: Fix : dto 클래스에 @Serializable 어노테이션 추가 -  직렬화를 위해 dto 클래스에 @Serializable 어노테이션 추가
    Related to: #153

--- AOS/app/src/main/java/com/softeer/team6four/data/remote/reservation/dto/ReservationRequestDto.kt
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/reservation/dto/ReservationRequestDto.kt
@@ -1,7 +1,10 @@
 package com.softeer.team6four.data.remote.reservation.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ReservationRequestDto(
-    val data: Any,
+    val data: Unit?,
     val message: String,
     val statusCode: Int,
     val timestamp: String
