07f71f45a39c68dc281c5d69264977c2be056c58
99winnmin <ojysep9987@gmail.com>
2024-02-24 00:44:02 +0900

    :sparkles: feat : 내 카밥 예약 내역, 상세정보 보기 통합 fragment 구현
    - 카밥 ID를 저장 및 업데이트하는 _chargerId MutableStateFlow와 공개 StateFlow 구현
    - 카밥 상세 정보를 관리하는 _myChargerDetail MutableStateFlow와 공개 StateFlow 구현
    - 카밥 ID를 업데이트하는 updateChargerId 함수 구현
    - fetchMyChargerDetail 함수 내에서 ChargerRepository를 통해 충전기 상세 정보를 비동기적으로 가져오는 로직 구현
    - 가져온 카밥 상세 정보를 _myChargerDetail 상태 플로우에 업데이트
    
    Related to: #157

---
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/charger/MyChargerDetailViewModel.kt
@@ -0,0 +1,59 @@
+package com.softeer.team6four.ui.mypage.charger
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softeer.team6four.data.Resource
+import com.softeer.team6four.data.local.UserPreferencesRepository
+import com.softeer.team6four.data.remote.charger.ChargerRepository
+import com.softeer.team6four.data.remote.charger.model.MyChargerDetailInfoModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MyChargerDetailViewModel @Inject constructor(
+    private val userPreferencesRepository: UserPreferencesRepository,
+    private val chargerRepository: ChargerRepository
+) : ViewModel(){
+    private val _chargerId: MutableStateFlow<Int> = MutableStateFlow(0)
+    val chargerId: MutableStateFlow<Int> = _chargerId
+
+    private val _myChargerDetail: MutableStateFlow<MyChargerDetailInfoModel> = MutableStateFlow(MyChargerDetailInfoModel())
+    val myChargerDetail: MutableStateFlow<MyChargerDetailInfoModel> = _myChargerDetail
+
+    fun updateChargerId(chargerId: Int) {
+        _chargerId.value = chargerId
+    }
+
+    fun fetchMyChargerDetail(chargerId: Int) {
+        viewModelScope.launch {
+            val accessToken = userPreferencesRepository.getAccessToken().first()
+            val myChargerDetailData = chargerRepository.fetchChargerDetailInfo(
+                accessToken = accessToken,
+                chargerId = chargerId
+            )
+
+            myChargerDetailData.catch {
+                Log.e("fetchChargerDetailInfo", "fetchMyChargerDetail: $it")
+            }.collect {resource ->
+                when (resource) {
+                    is Resource.Success -> {
+                        _myChargerDetail.value = resource.data
+                    }
+
+                    is Resource.Error -> {
+                        Log.e("fetchChargerDetailInfo", "fetchMyChargerDetail: ${resource.message}")
+                    }
+
+                    else -> {
+                        Log.e("fetchChargerDetailInfo", "fetchMyChargerDetail: loading")
+                    }
+                }
+            }
+        }
+    }
+}
