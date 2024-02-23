3443cf5f5d1050a74fdd09c9351c093aa8f88568
DSL2e <kdb2013@g.skku.edu>
2024-02-23 18:03:38 +0900

    :sparkles: feat: 테스트를 위한 api 추가
    - 테스트를 위한 api 추가
    
    Related to : #163

--- BE/src/main/java/com/softeer/team6four/global/consts/WhitePathsConst.java
+++ BE/src/main/java/com/softeer/team6four/global/consts/WhitePathsConst.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 public class WhitePathsConst {
 
 	public static final String[] WHITE_PATHS = {
-		"/", "/v1/user/auth/**"
+		"/health/check", "/", "/v1/user/auth/**"
 	};
 }
