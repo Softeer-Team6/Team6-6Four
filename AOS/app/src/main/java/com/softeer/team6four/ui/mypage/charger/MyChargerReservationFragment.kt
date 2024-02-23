1ecefdb2b840e85a017cf36f5cbb16bb7b774475
99winnmin <ojysep9987@gmail.com>
2024-02-24 00:38:31 +0900

    :sparkles: feat : 카밥 예약 목록 조회 및 예약 승인/거절 리스너 추가
    - ChargerReservationAdapter를 사용하여 충전기 예약 요청 목록을 RecyclerView에 표시하는 로직 구현
    - 예약 요청 항목 선택 시, 해당 예약 상세 정보를 ApproveReservationDialogViewModel에 업데이트하고, ApproveReservationDialogFragment를 표시하여 사용자에게 승인 또는 거절 선택을 제공하는 로직 구현
    - RecyclerView 스크롤 리스너를 추가하여 사용자가 목록 끝에 도달했을 때 추가 예약 요청 데이터를 로드하는 페이지네이션 기능 구현
    - myChargerReservationViewModel에서 제공하는 예약 목록 데이터를 관찰하고, 데이터가 없는 경우 빈 상태 화면을 표시하는 로직 구현
    - onReservationUpdated 콜백 메서드를 통해 예약 상태가 업데이트된 후 예약 목록을 reloading 로직 구현
    
    Related to: #157

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/charger/MyChargerReservationFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/charger/MyChargerReservationFragment.kt
@@ -5,10 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.softeer.team6four.databinding.FragmentMyChargerReservationBinding
+import com.softeer.team6four.ui.mypage.charger.adapter.ChargerReservationAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class MyChargerReservationFragment : Fragment() {
+@AndroidEntryPoint
+class MyChargerReservationFragment : Fragment(), ReservationUpdateCallback {
     private var _binding: FragmentMyChargerReservationBinding? = null
+    private val myChargerReservationViewModel: MyChargerReservationViewModel by activityViewModels()
+    private val approveReservationDialogViewModel: ApproveReservationDialogViewModel by activityViewModels()
     private val binding
         get() = _binding!!
 
@@ -22,11 +34,83 @@ class MyChargerReservationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.root.setOnClickListener { //Test Code
-            ApproveReservationDialogFragment().show(
-                childFragmentManager,
-                ApproveReservationDialogFragment.TAG
-            )
+
+        with(binding) {
+            viewModel = myChargerReservationViewModel
+            lifecycleOwner = viewLifecycleOwner
+
+            var count = 0
+
+            val adapter = ChargerReservationAdapter { reservationDetail ->
+                approveReservationDialogViewModel.updateReservationId(reservationDetail.reservationId)
+                approveReservationDialogViewModel.updateReservationDetail(
+                    reservationDetail.guestNickname,
+                    reservationDetail.rentalDate,
+                    reservationDetail.rentalTime,
+                    reservationDetail.totalFee
+                )
+                ApproveReservationDialogFragment().show(
+                    childFragmentManager,
+                    ApproveReservationDialogFragment.TAG
+                )
+            }
+            binding.rvChargerReservation.adapter = adapter
+
+            val layoutManager = LinearLayoutManager(context)
+            binding.rvChargerReservation.layoutManager = layoutManager
+
+            adapter.clearChargerReservationList()
+            myChargerReservationViewModel.fetchMyChargerReservationList(myChargerReservationViewModel.chargerId.value)
+
+            binding.rvChargerReservation.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    val lastVisibleItemPosition =
+                        (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
+                    val itemTotalCount = recyclerView.adapter!!.itemCount-1
+
+                    if (!binding.rvChargerReservation.canScrollVertically(1)
+                        && lastVisibleItemPosition == itemTotalCount
+                        && myChargerReservationViewModel.isLoading.value.not())
+                    {
+                        count++
+                        myChargerReservationViewModel.fetchMyChargerReservationList(
+                            myChargerReservationViewModel.chargerId.value,
+                            myChargerReservationViewModel.myChargerReservationList.value.lastOrNull()?.reservationId
+                        )
+                    }
+                }
+            })
+
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    launch {
+                        myChargerReservationViewModel.myChargerReservationList.collect { chargerReservationList ->
+                            if (chargerReservationList.isEmpty() && count == 0) {
+                                binding.rvChargerReservation.visibility = View.GONE
+                                binding.ivEmptyState.visibility = View.VISIBLE
+                                binding.tvEmptyPointHistory.visibility = View.VISIBLE
+                            } else {
+                                adapter.setChargerReservationList(chargerReservationList)
+                                if (chargerReservationList.isNotEmpty() && chargerReservationList.last().reservationId != 0) {
+                                    adapter.removeLoadingFooter()
+                                }
+                                binding.rvChargerReservation.visibility = View.VISIBLE
+                                binding.ivEmptyState.visibility = View.GONE
+                                binding.tvEmptyPointHistory.visibility = View.GONE
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onReservationUpdated(updated: Boolean, reservationId: Int?) {
+        if (updated && reservationId != null) {
+            val adapter = binding.rvChargerReservation.adapter as ChargerReservationAdapter
+            adapter.clearChargerReservationList()
         }
     }
 
