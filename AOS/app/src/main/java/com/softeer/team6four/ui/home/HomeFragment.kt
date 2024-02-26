d60be89aaac1f57f48e398c417b66f9d3bb25fa6
kimseongwooo <tjddn929@naver.com>
2024-02-26 17:27:33 +0900

    :sparkles: Feat : HomeFragment 라이프사이클 콜백 함수에 따라 네이버 맵 리소스 관리 - HomeFragment 라이프사이클 콜백 함수에 따라 네이버 맵 리소스 관리
    Related to: #153

--- AOS/app/src/main/java/com/softeer/team6four/ui/home/HomeFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/home/HomeFragment.kt
@@ -122,6 +122,41 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
     private fun setNavigationDrawer() {
         with(binding) {
             btnMenu.setOnClickListener { drawerLayout.open() }
