cc3b65e429dbd7d7bf11f15ba926b79ff97d00e8
kimseongwooo <tjddn929@naver.com>
2024-02-27 00:29:45 +0900

    :sparkles: Feat : PaymentSuccessFragment 클래스 생성 - 결제 성공 시 나타나는 화면을 담당하는 PaymentSuccessFragment 클래스 생성 - 레이아웃 구성 - 데이터 바인딩 적용
    Related to: #176

---
+++ AOS/app/src/main/java/com/softeer/team6four/ui/payment/PaymentSuccessFragment.kt
@@ -0,0 +1,41 @@
+package com.softeer.team6four.ui.payment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.softeer.team6four.databinding.FragmentPaymentSuccessBinding
+
+class PaymentSuccessFragment : Fragment() {
+    private var _binding: FragmentPaymentSuccessBinding? = null
+    private val paymentViewModel : PaymentViewModel by activityViewModels()
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPaymentSuccessBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = paymentViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.ibCancel.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
